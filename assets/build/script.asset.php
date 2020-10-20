28cb761f6d0a05c70dfdb3126878eb93e7c33376
Michael Auteri <mauteri@gmail.com>
2020-10-20 11:40:28 -0400

    GP-11: Fix default navigation so it is attending.

--- assets/build/script.asset.php
+++ assets/build/script.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '7a2a1bc504948186dee97aebeb63ffdb');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'c94b667ff76bb7bc4799ffba4df1ffd2');
