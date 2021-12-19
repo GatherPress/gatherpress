0a4ae9c79269ca467571ade1af2b6089e9e8804a
Michael Auteri <mauteri@gmail.com>
2021-12-19 11:20:31 -0500

    GP-18: Disable Attendance Selector if not logged in or event has past.

--- templates/blocks/attendance-selector.php
+++ templates/blocks/attendance-selector.php
@@ -7,6 +7,11 @@
  * @since 1.0.0
  */
 
+$event = new GatherPress\Core\Event( get_the_ID() );
+
+if ( ! is_user_logged_in() || $event->has_event_past() ) {
+	return;
+}
 ?>
 
 <div id="gp-attendance-selector-container"></div>
