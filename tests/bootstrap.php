f26179e71b0706f0a01fe4975a29a48af878fa8e
Michael Auteri <mauteri@gmail.com>
2022-02-15 23:15:04 -0500

    Start fixing unit tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,8 +10,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 $instance = PMC\Unit_Test\Bootstrap::get_instance();
 
 tests_add_filter( 'plugins_loaded', function() {
-	wpcom_vip_load_plugin( 'buddypress-vip-go' );
-
 	// Manually load our plugin without having to setup the development folder in the correct plugin folder
 	require_once __DIR__ . '/../gatherpress.php';
 } );
