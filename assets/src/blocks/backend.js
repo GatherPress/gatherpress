d34806c0d66cd64ebbbea0dcfff4e798edb0fd71
Michael Auteri <mauteri@gmail.com>
2022-02-25 20:55:41 -0500

    GP-78: Update logo to nametag dashicon for GatherPress.

--- assets/src/blocks/backend.js
+++ assets/src/blocks/backend.js
@@ -6,10 +6,6 @@ import './attendance-selector/index';
 import './upcoming-events/index';
 import './past-events/index';
 
-// @todo update svg code to new logo
-import gatherPressIcon from './../../images/gatherpress-icon';
-
-// alter the icon slot
 wp.blocks.updateCategory( 'gatherpress', {
-	icon: gatherPressIcon
+	icon: 'nametag'
 });
