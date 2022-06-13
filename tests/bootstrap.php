2dc0d7480b482521d66dc6a697dbf299ab8a6610
Michael Auteri <mauteri@gmail.com>
2022-06-13 11:08:14 -0400

    Debugging.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,8 +5,10 @@
  * @package GatherPress
  * @subpackage Tests
  * @since 1.0.0
- *
  */
+
+// phpcs:disable Squiz.Commenting.FileComment.Missing
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $gatherpress_bootstrap_instance = PMC\Unit_Test\Bootstrap::get_instance();
