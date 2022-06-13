cd3ae130fa666fd92c7a634424c6b752bd3c62e3
Michael Auteri <mauteri@gmail.com>
2022-06-13 12:09:22 -0400

    PHPCS cleanup.

--- core/classes/class-cli.php
+++ core/classes/class-cli.php
@@ -24,18 +24,34 @@ use WP_CLI;
  */
 class CLI extends WP_CLI {
 
+	/**
+	 * Make changes to an event.
+	 *
+	 * @param array $args       Arguments of the script.
+	 * @param array $assoc_args Associative arguments of the script.
+	 *
+	 * @return void
+	 */
 	public function event( array $args = array(), array $assoc_args = array() ) {
 		$event_id = (int) $args[0];
 		$action   = (string) $args[1];
 
-		switch( $action ) {
+		switch ( $action ) {
 			case 'add-attendee':
 				$this->add_attendee( $event_id, $assoc_args );
 				break;
 		}
 	}
 
-	private function add_attendee( $event_id, $assoc_args ) {
+	/**
+	 * Add an attendee to an event.
+	 *
+	 * @param int   $event_id   Post ID of the event.
+	 * @param array $assoc_args Associative arguments for script.
+	 *
+	 * @return void
+	 */
+	private function add_attendee( int $event_id, array $assoc_args ) {
 		$event   = new Event( $event_id );
 		$user_id = $assoc_args['user_id'];
 		$status  = $assoc_args['status'];
@@ -45,6 +61,7 @@ class CLI extends WP_CLI {
 
 		\WP_CLI::success( $response );
 	}
+
 }
 
 \WP_CLI::add_command( 'gatherpress', CLI::class );
