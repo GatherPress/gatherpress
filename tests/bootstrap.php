8ac38d3985fd7fe038d68814d49895b6c7f961c7
Michael Auteri <mauteri@gmail.com>
2022-06-13 11:03:26 -0400

    Testing.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,12 @@
 <?php
 /**
- * PHPUnit bootstrap file
- *
- * Bootstrap file for plugin automated tests.
+ * PHPUnit bootstrap file.
  *
  * @package GatherPress
  * @subpackage Tests
  * @since 1.0.0
+ *
  */
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $gatherpress_bootstrap_instance = PMC\Unit_Test\Bootstrap::get_instance();
