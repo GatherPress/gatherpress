4bce0437ba15a972753518d13c91e2081e746fb5
Michael Auteri <mauteri@gmail.com>
2022-07-16 15:32:55 -0400

    Small thing to help Caleb with Event Date block.

--- templates/blocks/event-date.php
+++ templates/blocks/event-date.php
@@ -6,7 +6,7 @@
  * @subpackage Core
  * @since 1.0.0
  */
+
 $gatherpress_event = new \GatherPress\Core\Event( get_the_ID() );
 ?>
 <div><?php echo esc_html( $gatherpress_event->get_display_datetime() ); ?></div>
-<div>Event Date</div>
