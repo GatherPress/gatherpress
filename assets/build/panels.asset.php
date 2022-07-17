c7997a31a169953396939b3daac9edd9dd4ee1e0
Michael Auteri <mauteri@gmail.com>
2022-07-17 16:30:51 -0400

    Build.

--- assets/build/panels.asset.php
+++ assets/build/panels.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '28e95c8d05727a4977a8');
+<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '223457230458bb6c7891');
