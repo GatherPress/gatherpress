362220febbcb8de79134c02540694bcdc962d543
Michael Auteri <mauteri@gmail.com>
2022-08-27 11:42:01 -0400

    Bug fix. Default event in event list block was object vs array so map function through an error.

--- assets/build/blocks_frontend.asset.php
+++ assets/build/blocks_frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2260cdc5fa9f690a97d5');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '01251cebaf449fd735f1');
