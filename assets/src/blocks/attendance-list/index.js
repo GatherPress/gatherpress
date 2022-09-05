4d696e23f26ddae2fa6a8131a0e3a5c0b4204224
Paul Barthmaier <pbrocks@users.noreply.github.com>
2022-09-05 12:00:31 -0400

    attendance list block json

--- assets/src/blocks/attendance-list/index.js
+++ assets/src/blocks/attendance-list/index.js
@@ -1,17 +1,11 @@
 import { __ } from '@wordpress/i18n';
-import { registerBlockType } from '@wordpress/blocks';
-import Edit from './edit';
+import edit from './edit';
 
-registerBlockType( 'gatherpress/attendance-list', {
-	apiVersion: 2,
-	title: __( 'Attendance List', 'gatherpress' ),
-	icon: 'groups',
-	category: 'gatherpress',
-	attributes: {
-		blockId: { type: 'string' },
-		content: { type: 'string' },
-		color: { type: 'string' },
-	},
-	edit: Edit,
+import './style.scss';
+
+import metadata from './block.json';
+
+registerBlockType ( metadata, {
+	edit,
 	save: () => null,
 } );
