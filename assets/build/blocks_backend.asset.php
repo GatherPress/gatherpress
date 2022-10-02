4f6ddef12cd49193a03c65a6fc5666142788c76b
Michael Auteri <mauteri@gmail.com>
2022-10-02 16:58:38 -0400

    Fix display of attendance selector block.

--- assets/build/blocks_backend.asset.php
+++ assets/build/blocks_backend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a1a2061cf0c66c36d1c5');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a55a5638533d3b20d868');
--- assets/src/blocks/attendance-selector/edit.js
+++ assets/src/blocks/attendance-selector/edit.js
@@ -10,8 +10,6 @@ import AttendanceSelector from '../../components/AttendanceSelector';
 const Edit = () => {
 	const blockProps = useBlockProps();
 	// eslint-disable-next-line no-undef
-	const type = ( '1' === GatherPress.has_event_past ) ? 'past' : 'upcoming';
-	// eslint-disable-next-line no-undef
 	const postId = GatherPress.post_id;
 	// eslint-disable-next-line no-undef
 	const currentUser = GatherPress.current_user;
@@ -21,7 +19,7 @@ const Edit = () => {
 			<AttendanceSelector
 				eventId={ postId }
 				currentUser={ currentUser }
-				type={ type }
+				type={ 'upcoming' }
 			/>
 		</div>
 	);
