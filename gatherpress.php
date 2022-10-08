e03bf557f8c9018501b50ee6c7dfce12322896e6
Paul Barthmaier <pbrocks@users.noreply.github.com>
2022-10-08 13:57:41 -0400

    Update gatherpress.php

--- gatherpress.php
+++ gatherpress.php
@@ -30,3 +30,40 @@ require_once GATHERPRESS_CORE_PATH . '/core/classes/class-autoloader.php';
 GatherPress\Core\Autoloader::register();
 GatherPress\Core\Setup::get_instance();
 GatherPress\BuddyPress\Setup::get_instance();
+
+
+register_activation_hook( __FILE__, 'activate_gatherpress_plugin' );
+/**
+ * Activate GatherPress plugin.
+ *
+ * @return void
+ */
+function activate_gatherpress_plugin() {
+	// $setup = GatherPress\Core\Setup::register_post_types();
+	// $flushing = $setup->register_post_types();
+	if ( ! defined( 'GATHERPRESS_VERSION' ) ) {
+		add_option( 'gatherpress_flush_rewrite_rules_flag', true );
+	}
+}
+
+register_deactivation_hook( __FILE__, 'deactivate_gatherpress_plugin' );
+/**
+ * Activate GatherPress plugin.
+ *
+ * @return void
+ */
+function deactivate_gatherpress_plugin() {
+	flush_rewrite_rules();
+}
+
+add_action( 'init', 'maybe_flush_gatherpress_rewrite_rules', 20 );
+/**
+ * Flush rewrite rules if the previously added flag exists,
+ * and then remove the flag.
+ */
+function maybe_flush_gatherpress_rewrite_rules() {
+	if ( get_option( 'gatherpress_flush_rewrite_rules_flag' ) ) {
+		flush_rewrite_rules();
+		delete_option( 'gatherpress_flush_rewrite_rules_flag' );
+	}
+}
