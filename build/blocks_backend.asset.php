ceaa423e988744e9d56f901c803bc184a6bb0d60
Paul Barthmaier <pbrocks@users.noreply.github.com>
2022-10-27 14:05:31 -0400

    removing icon from JS

--- build/blocks_backend.asset.php
+++ build/blocks_backend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '11ce214a28fb2eb20915');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ea5ffe32cf5ffdef9112');
