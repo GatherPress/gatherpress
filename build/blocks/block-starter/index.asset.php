d0051bed3f62c76e1f0eebf8e10999404fa1eb48
Paul Barthmaier <pbrocks@users.noreply.github.com>
2022-10-28 23:23:15 -0400

    remove tests

--- build/blocks/block-starter/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '3b60531aaa6aabfdd46b');
--- build/blocks/example-dynamic/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '649ecd9bc61af40bdce6');
