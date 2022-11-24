343e884eb68e14d0c2cf9988d702912338fb82a0
Michael Auteri <mauteri@gmail.com>
2022-11-24 08:34:25 -0500

    Update NodeJS.

--- build/blocks_frontend.asset.php
+++ build/blocks_frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'df1afdb5790850ce0501');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '91a3297517a5bc0d8724');
