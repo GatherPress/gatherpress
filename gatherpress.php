96abbc28c21ca40b8fcf439816525611fb09f6a7
Michael Auteri <mauteri@gmail.com>
2022-11-24 09:20:03 -0500

    Updates.

--- gatherpress.php
+++ gatherpress.php
@@ -5,7 +5,7 @@
  * Description:         GatherPress adds event management and more to WordPress.
  * Author:              The GatherPress Community
  * Author URI:          https://gatherpess.org/
- * Version:             0.12.0
+ * Version:             0.12.1
  * Minimum PHP Version: 7.3
  * Text Domain:         gatherpress
  * License:             GPLv2 or later (license.txt)
