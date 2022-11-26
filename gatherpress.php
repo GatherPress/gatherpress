90efa9a6e78ddb720adfef4e67f03b54fc2339c0
Michael Auteri <mauteri@gmail.com>
2022-11-26 18:50:09 -0500

    GP-116: Added archive page configurations for past and upcoming events.

--- gatherpress.php
+++ gatherpress.php
@@ -5,7 +5,7 @@
  * Description:         Powering Communities with WordPress.
  * Author:              The GatherPress Community
  * Author URI:          https://gatherpess.org/
- * Version:             0.12.2
+ * Version:             0.13.0
  * Minimum PHP Version: 7.3
  * Text Domain:         gatherpress
  * License:             GPLv2 or later (license.txt)
---
+++ src/components/Autocomplete.js
@@ -0,0 +1,100 @@
+/**
+ * External dependencies.
+ */
+import { includes } from 'lodash';
+
+/**
+ * WordPress dependencies.
+ */
+import { FormTokenField } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { useState } from '@wordpress/element';
+import { store as coreStore } from '@wordpress/core-data';
+import { useSelect } from '@wordpress/data';
+
+const Autocomplete = ( props ) => {
+	const { name, option, value, fieldOptions } = props.attrs;
+	const [ content, setContent ] = useState( JSON.parse( value ) ?? '[]' );
+	const { contentList } = useSelect(
+		( select ) => {
+			const { getEntityRecords } = select( coreStore );
+			const entityType = ( 'user' !== fieldOptions.type ) ? 'postType' : 'root';
+			const kind = fieldOptions.type || 'post';
+			return {
+				contentList: getEntityRecords( entityType, kind, {
+					per_page: -1,
+					context: 'view',
+				} ),
+			};
+		},
+		[ content ]
+	);
+
+	const contentSuggestions =
+		contentList?.reduce(
+			( accumulator, content ) => ( {
+				...accumulator,
+				[ content.title?.rendered || content.name ]: content,
+			} ),
+			{}
+		) ?? {};
+
+	const selectContent = ( tokens ) => {
+		const hasNoSuggestion = tokens.some(
+			( token ) => typeof token === 'string' && ! contentSuggestions[ token ]
+		);
+
+		if ( hasNoSuggestion ) {
+			return;
+		}
+
+		const allContent = tokens.map( ( token ) => {
+			return typeof token === 'string' ? contentSuggestions[ token ] : token;
+		} );
+
+		if ( includes( allContent, null ) ) {
+			return false;
+		}
+
+		setContent( allContent );
+	};
+
+	return (
+		<>
+			<FormTokenField
+				key={ option }
+				label={ fieldOptions.label || __( 'Select Posts', 'gatherpress' ) }
+				name={ name }
+				value={
+					content &&
+					content.map( ( item ) => ( {
+						id: item.id,
+						slug: item.slug,
+						value: item.title?.rendered || item.name || item.value,
+					} ) )
+				}
+				suggestions={ Object.keys( contentSuggestions ) }
+				onChange={ selectContent }
+				maxSuggestions={ fieldOptions.max_suggestions || 20 }
+				maxLength={ fieldOptions.limit || 0 }
+			/>
+			<input
+				type="hidden"
+				id={ option }
+				name={ name }
+				value={
+					content &&
+					JSON.stringify(
+						content.map( ( item ) => ( {
+							id: item.id,
+							slug: item.slug,
+							value: item.title?.rendered || item.name || item.value,
+						} ) )
+					)
+				}
+			/>
+		</>
+	);
+};
+
+export default Autocomplete;
