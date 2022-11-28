525407a882318a66713e38cdea99ed99787e986a
Jeffrey Marx <jeffrey_marx@yahoo.com>
2022-11-27 21:01:01 -0500

    Force localized script in the header

--- includes/core/classes/class-assets.php
+++ includes/core/classes/class-assets.php
@@ -56,7 +56,7 @@ class Assets {
 	protected function setup_hooks() {
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ), 10, 1 );
-		add_action( 'enqueue_block_editor_assets', array( $this, 'block_enqueue_scripts' ), 1 );
+		add_action( 'enqueue_block_editor_assets', array( $this, 'block_enqueue_scripts' ), 9 );
 	}
 
 	/**
@@ -175,8 +175,7 @@ class Assets {
 			'gatherpress-blocks-backend',
 			$this->build . 'blocks_backend.js',
 			$asset['dependencies'],
-			$asset['version'],
-			true
+			$asset['version']
 		);
 
 		wp_localize_script(
