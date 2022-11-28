b553c25b6b63638528e1b5c5f0bbdd61d755efd5
Michael Auteri <mauteri@gmail.com>
2022-11-27 22:27:03 -0500

    Revert old file.

--- src/panels/helpers.js
+++ src/panels/helpers.js
@@ -7,8 +7,6 @@ import moment from 'moment';
  * WordPress dependencies.
  */
 import { __ } from '@wordpress/i18n';
-import { store as noticesStore } from '@wordpress/notices';
-import { useDispatch } from '@wordpress/data';
 
 // Checks if the post type is for events.
 export function isEventPostType() {
@@ -27,24 +25,19 @@ export function enableSave() {
 
 export function hasEventPastNotice() {
 	const id = 'gp_event_past';
-	// const notices = wp.data.dispatch( 'core/notices' );
-	const { createErrorNotice } = useDispatch( noticesStore );
+	const notices = wp.data.dispatch( 'core/notices' );
 
 	notices.removeNotice( id );
 
 	if ( hasEventPast() ) {
-		createErrorNotice( __( 'This event has already past.', 'gatherpress' ), {
-			type: id,
-			explicitDismiss: true,
-		} );
-		// notices.createNotice(
-		// 	'warning',
-		// 	__( 'This event has already past.', 'gatherpress' ),
-		// 	{
-		// 		id,
-		// 		isDismissible: false,
-		// 	}
-		// );
+		notices.createNotice(
+			'warning',
+			__( 'This event has already past.', 'gatherpress' ),
+			{
+				id,
+				isDismissible: true,
+			}
+		);
 	}
 }
 
