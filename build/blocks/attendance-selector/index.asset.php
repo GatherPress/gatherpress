cb97d1d7a87d9a610346b98d7955a1df99ca841f
Paul Barthmaier <pbrocks@users.noreply.github.com>
2022-12-23 12:12:33 -0500

    frontend attendance selector working

--- build/blocks/attendance-selector/index.asset.php
+++ build/blocks/attendance-selector/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '564842f6faed53807d93');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '72705538d6db21849ba4');
