62ac5a8b474f7b86373fdafc9e139b7b2c856a3e
Paul Barthmaier <pbrocks@users.noreply.github.com>
2022-12-24 23:23:24 -0500

    Update render.php

--- src/blocks/attendance-selector/render.php
+++ src/blocks/attendance-selector/render.php
@@ -11,10 +11,10 @@ if ( ! isset( $attributes ) || ! is_array( $attributes ) ) {
 	return;
 }
 
-$attendance_selector = get_block_wrapper_attributes();
+$gp_attendance_selector = get_block_wrapper_attributes();
 if ( $attributes['className'] ) {
-	$attendance_selector = 'class="' . $attributes['className'] . '"';
+	$gp_attendance_selector = 'class="' . $attributes['className'] . '"';
 }
 ?>
 
-<div <?php echo $attendance_selector; ?> data-gp_block_name="attendance-selector" data-gp_block_attrs="<?php echo esc_attr( htmlspecialchars( wp_json_encode( $attributes ), ENT_QUOTES, 'UTF-8' ) ); ?>"></div>
+<div <?php echo $gp_attendance_selector; ?> data-gp_block_name="attendance-selector" data-gp_block_attrs="<?php echo esc_attr( htmlspecialchars( wp_json_encode( $attributes ), ENT_QUOTES, 'UTF-8' ) ); ?>"></div>
