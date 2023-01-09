2e66f44c7d072fe1d89f395b283a0265023ca194
Paul Barthmaier <pbrocks@users.noreply.github.com>
2023-01-09 10:32:32 -0500

    formatting php

--- src/blocks/events-list/render.php
+++ src/blocks/events-list/render.php
@@ -2,14 +2,14 @@
 /**
  * Render Events List block.
  *
- * @package GatherPress
+ * @package    GatherPress
  * @subpackage Core
- * @since 1.0.0
+ * @since      1.0.0
  */
 
-if ( ! isset( $attributes ) || ! is_array( $attributes ) ) {
-	return;
+if (! isset($attributes) || ! is_array($attributes) ) {
+    return;
 }
 ?>
 
-<div <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?> data-gp_block_name="events-list" data-gp_block_attrs="<?php echo esc_attr( htmlspecialchars( wp_json_encode( $attributes ), ENT_QUOTES, 'UTF-8' ) ); ?>"></div>
+<div <?php echo wp_kses_data(get_block_wrapper_attributes()); ?> data-gp_block_name="events-list" data-gp_block_attrs="<?php echo esc_attr(htmlspecialchars(wp_json_encode($attributes), ENT_QUOTES, 'UTF-8')); ?>"></div>
