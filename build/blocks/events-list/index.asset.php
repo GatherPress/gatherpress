d712f48eb3ed6e345c078079e9145b6472c40a0e
Paul Barthmaier <pbrocks@users.noreply.github.com>
2023-01-11 23:57:46 -0500

    linting

--- build/blocks/events-list/index.asset.php
+++ build/blocks/events-list/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5cc629f2d9faa7c10a23');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '68588882e20352449bd6');
--- build/blocks/venue/render.php
+++ build/blocks/venue/render.php
@@ -10,25 +10,25 @@
 use GatherPress\Core\Utility;
 use GatherPress\Core\Venue;
 
-if ( ! isset($attributes) || ! is_array( $attributes ) ) {
-    return;
+if ( ! isset( $attributes ) || ! is_array( $attributes ) ) {
+	return;
 }
 
 $gatherpress_venue = get_post( intval( $attributes['venueId'] ?? 0 ) );
 
-if ( Venue::POST_TYPE !== get_post_type($gatherpress_venue ) ) {
-    return;
+if ( Venue::POST_TYPE !== get_post_type( $gatherpress_venue ) ) {
+	return;
 }
 
 // phpcs:ignore
 $gp_venue_map = ( get_post( $gatherpress_venue->ID )->post_content ?: '' );
 
 printf(
-    '<div>%s</div>',
-    wp_kses(
-        $gp_venue_map,
-        array(
-			'iframe'              => array(
+	'<div>%s</div>',
+	wp_kses(
+		$gp_venue_map,
+		array(
+			'iframe' => array(
 				'src'             => array(),
 				'width'           => array(),
 				'height'          => array(),
@@ -37,6 +37,6 @@ printf(
 				'allowfullscreen' => array(),
 				'frameborder'     => array(),
 			),
-        )
-    )
+		)
+	)
 );
