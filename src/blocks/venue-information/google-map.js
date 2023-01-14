567179ae1be434a3ac35acd89eeface24a298538
Paul Barthmaier <pbrocks@users.noreply.github.com>
2023-01-14 11:33:09 -0500

    src changes

---
+++ src/blocks/venue-information/google-map.js
@@ -0,0 +1,23 @@
+const GoogleMapEmbed = (props) => {
+	const { location, zoom, type, height, className } = props;
+	const style = { border: 0, height, width: '100%' };
+	const baseUrl = 'https://maps.google.com/maps';
+
+	const params = new URLSearchParams({
+		q: location,
+		z: zoom || 1,
+		t: type,
+		output: 'embed',
+	});
+
+	const src = baseUrl + '?' + params.toString();
+	return (
+		<iframe
+			src={src}
+			style={style}
+			className={className}
+			title={location}
+		></iframe>
+	);
+};
+export default GoogleMapEmbed;
--- src/blocks/venue/render.php
+++ src/blocks/venue/render.php
@@ -2,9 +2,9 @@
 /**
  * Render Venue block.
  *
- * @package GatherPress
+ * @package    GatherPress
  * @subpackage Core
- * @since 1.0.0
+ * @since      1.0.0
  */
 
 use GatherPress\Core\Utility;
@@ -20,17 +20,24 @@ if ( Venue::POST_TYPE !== get_post_type( $gatherpress_venue ) ) {
 	return;
 }
 
-$gatherpress_venue_information = json_decode( get_post_meta( $gatherpress_venue->ID, '_venue_information', true ) );
+// phpcs:ignore
+$gp_venue_map = ( get_post( $gatherpress_venue->ID )->post_content ?: '' );
 
-Utility::render_template(
-	sprintf( '%s/build/blocks/venue-information/render.php', GATHERPRESS_CORE_PATH ),
-	array(
-		'attributes' => array(
-			'name'        => $gatherpress_venue->post_title,
-			'fullAddress' => $gatherpress_venue_information->fullAddress ?? '', // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
-			'phoneNumber' => $gatherpress_venue_information->phoneNumber ?? '', // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
-			'website'     => $gatherpress_venue_information->website ?? '',
-		),
-	),
-	true
+printf(
+	'<div %s>%s</div>',
+	wp_kses_data( get_block_wrapper_attributes() ),
+	wp_kses(
+		$gp_venue_map,
+		array(
+			'iframe' => array(
+				'src'             => array(),
+				'width'           => array(),
+				'height'          => array(),
+				'title'           => array(),
+				'allow'           => array(),
+				'allowfullscreen' => array(),
+				'frameborder'     => array(),
+			),
+		)
+	)
 );
