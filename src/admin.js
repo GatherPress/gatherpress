6c338831896d59ce38d1471aa82081225bd34e95
Paul Barthmaier <pbrocks@users.noreply.github.com>
2023-01-21 14:06:41 -0500

    ran PHPCBF on these 3 files

--- src/admin.js
+++ src/admin.js
@@ -7,14 +7,17 @@ import { getBlockType, unregisterBlockType } from '@wordpress/blocks';
 /**
  * Remove unwanted blocks from localized array.
  */
-domReady(() => {
-	// eslint-disable-next-line no-undef
-	Object.keys(GatherPress.unregister_blocks).forEach((key) => {
+domReady(
+	() => {
 		// eslint-disable-next-line no-undef
-		const blockName = GatherPress.unregister_blocks[key];
-
-		if (blockName && 'undefined' !== typeof getBlockType(blockName)) {
-			unregisterBlockType(blockName);
-		}
-	});
-});
+		Object.keys( GatherPress.unregister_blocks ).forEach(
+			(key) => {
+				// eslint-disable-next-line no-undef
+				const blockName = GatherPress.unregister_blocks[key];
+				if (blockName && 'undefined' !== typeof getBlockType( blockName )) {
+					unregisterBlockType( blockName );
+				}
+			}
+		);
+	}
+);
