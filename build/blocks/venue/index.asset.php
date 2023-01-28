edcb285d67e1b4282b09762b4709a0fd7d7316cd
Paul Barthmaier <pbrocks@users.noreply.github.com>
2023-01-28 11:35:57 -0500

    flex-start

--- build/blocks/venue/index.asset.php
+++ build/blocks/venue/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '8b09a09f21bd1649c820');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '522c8ffcb5bada5ef8d2');
