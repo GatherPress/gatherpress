c754ff70038e44d4a7c0cb34db085bdac8d8802b
Paul Barthmaier <pbrocks@users.noreply.github.com>
2023-01-28 12:03:01 -0500

    Venue => Event Venue list

--- build/blocks/venue/index.asset.php
+++ build/blocks/venue/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '522c8ffcb5bada5ef8d2');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '511a092d63c171fe05d9');
