631214ddd2c5bbfcf9cd221f7ffc04942fc1c722
Michael Auteri <mauteri@gmail.com>
2023-02-09 18:50:40 -0500

    Fix Event List block. Venue is messed up.

--- build/blocks/events-list/events-list.asset.php
+++ build/blocks/events-list/events-list.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '83c1a701b1d0a27abb4b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '3ff5e06fc49dc0ca4492');
--- build/blocks/events-list/index.asset.php
+++ build/blocks/events-list/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7513d2abc36cc8b73b1f');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '1a6a1e70201fb2c8f85f');
