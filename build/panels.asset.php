ea24b085110418d56af9f8c72b7f53ab575486ca
Michael Auteri <mauteri@gmail.com>
2023-02-12 18:08:16 -0500

    Fix issue with special characters in venue dropdown.

--- build/panels.asset.php
+++ build/panels.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '4a33e5e9568f9272da56');
+<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '4ab8201c0cc8a2853029');
