110b3e433d689c6ef210432b942ec64d6a8daea1
Paul Barthmaier <pbrocks@users.noreply.github.com>
2023-02-15 00:12:18 -0500

    code in index js confused things, as in 2 blocks

--- build/blocks/event-venue/index.asset.php
+++ build/blocks/event-venue/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3baf2528bba034baaf0f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a129302de0e29374d9da');
