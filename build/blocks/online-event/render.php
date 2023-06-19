3f43e36c2138bbf5c322d9a6ccf16d3fd3d527c3
Mike Auteri <mauteri@gmail.com>
2023-06-19 15:58:58 -0400

    Revert "Refactor online event block code"
    This reverts commit 0d497d7a6d3b4a222a07fe50ea1482629bc5767a.

--- build/blocks/online-event/render.php
+++ build/blocks/online-event/render.php
@@ -13,8 +13,33 @@ if ( ! isset( $attributes ) || ! is_array( $attributes ) ) {
 	return;
 }
 
-$gatherpress_event             = new Event( get_the_ID() );
-$attributes['onlineEventLink'] = $gatherpress_event->validate_online_event_link( $attributes['onlineEventLink'] ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+$gatherpress_post = get_post();
+
+if ( ! is_a( $gatherpress_post, 'WP_Post' ) ) {
+	return;
+}
+
+$gatherpress_user_id     = get_current_user_id();
+$gatherpress_online_link = $attributes['onlineEventLink'];
+$gatherpress_event       = new Event( $gatherpress_post->ID );
+
+$attributes['onlineEventLink'] = ''; // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+
+if (
+	! empty( $gatherpress_user_id ) &&
+	! empty( $gatherpress_online_link ) &&
+	is_object( $gatherpress_event->attendee )
+) {
+	$gatherpress_user = $gatherpress_event->attendee->get( $gatherpress_user_id );
+
+	// Only show online link if member is attending event.
+	if (
+		'attending' === $gatherpress_user['status'] &&
+		! $gatherpress_event->has_event_past()
+	) {
+		$attributes['onlineEventLink'] = $gatherpress_online_link; // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+	}
+}
 ?>
 
 <div <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?> data-gp_block_name="online-event" data-gp_block_attrs="<?php echo esc_attr( htmlspecialchars( wp_json_encode( $attributes ), ENT_QUOTES, 'UTF-8' ) ); ?>"></div>
--- src/blocks/online-event/edit.js
+++ src/blocks/online-event/edit.js
@@ -4,6 +4,8 @@
 import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
 import { Flex, FlexItem, TextControl } from '@wordpress/components';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { useEffect } from '@wordpress/element';
 
 /**
  * Internal dependencies.
@@ -13,7 +15,26 @@ import { getFromGlobal } from '../../helpers/globals';
 
 const Edit = ({ attributes, setAttributes, isSelected }) => {
 	const blockProps = useBlockProps();
+	const editPost = useDispatch('core/editor').editPost;
 	const { onlineEventLink } = attributes;
+	const onlineEventLinkMetaData = useSelect(
+		(select) =>
+			select('core/editor').getEditedPostAttribute('meta')
+				._online_event_link
+	);
+
+	const onUpdate = (value) => {
+		const meta = { _online_event_link: value };
+
+		setAttributes({ onlineEventLink: value });
+		editPost({ meta });
+	};
+
+	useEffect(() => {
+		setAttributes({
+			onlineEventUrl: onlineEventLinkMetaData ?? '',
+		});
+	}, [setAttributes, onlineEventLinkMetaData]);
 
 	return (
 		<div {...blockProps}>
@@ -28,7 +49,7 @@ const Edit = ({ attributes, setAttributes, isSelected }) => {
 								'gatherpress'
 							)}
 							onChange={(value) => {
-								setAttributes({ onlineEventLink: value });
+								onUpdate(value);
 							}}
 						/>
 					</FlexItem>
