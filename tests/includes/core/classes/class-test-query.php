ef91dd2939d6be130198fb1551466ed89a7245f0
Mike Auteri <mauteri@gmail.com>
2023-07-03 08:37:02 -0400

    Refactor Query class method and add unit tests

--- tests/includes/core/classes/class-test-query.php
+++ tests/includes/core/classes/class-test-query.php
@@ -9,6 +9,7 @@
 
 namespace GatherPress\Tests\Core;
 
+use GatherPress\Core\Event;
 use GatherPress\Core\Query;
 use PMC\Unit_Test\Base;
 
@@ -20,7 +21,7 @@ use PMC\Unit_Test\Base;
 class Test_Query extends Base {
 
 	/**
-	 * Coverage for setup_hooks.
+	 * Coverage for setup_hooks method.
 	 *
 	 * @covers ::__construct
 	 * @covers ::setup_hooks
@@ -45,6 +46,80 @@ class Test_Query extends Base {
 		);
 
 		$this->assert_hooks( $hooks, $instance );
+	}
+
+	/**
+	 * Coverage for get_upcoming_events method.
+	 *
+	 * @covers ::get_upcoming_events
+	 * @covers ::order_upcoming_events
+	 * @covers ::get_events_list
+	 * @covers ::pre_get_posts
+	 *
+	 * @return void
+	 */
+	public function test_get_upcoming_events(): void {
+		$instance = Query::get_instance();
+		$response = $instance->get_upcoming_events();
+
+		$this->assertEmpty( $response->posts, 'Failed to assert that posts array is empty.' );
+		$this->assertSame( 5, $response->query[ 'posts_per_page' ], 'Failed to assert post per page limit.' );
+
+		$post  = $this->mock->post( array( 'post_type' => 'gp_event' ) )->get();
+		$event = new Event( $post->ID );
+		$date  = new \DateTime( 'tomorrow' );
+
+		$params = array(
+			'datetime_start' => $date->format( 'Y-m-d H:i:s' ),
+			'datetime_end'   => $date->modify( '+1 day' )->format( 'Y-m-d H:i:s' ),
+			'timezone'       => 'America/New_York',
+		);
+
+		$event->save_datetimes( $params );
+
+		$response = $instance->get_upcoming_events( 1 );
+
+		$this->assertSame( $response->posts[0], $post->ID, 'Failed to assert that event ID is in array.' );
+		$this->assertSame( 1, $response->query[ 'posts_per_page' ], 'Failed to assert post per page limit.' );
+		$this->assertSame( 'upcoming', $response->query[ 'gp_events_query' ], 'Failed to assert query is upcoming.' );
+		$this->assertSame( 'gp_event', $response->query[ 'post_type' ], 'Failed to assert post type is gp_event.' );
+	}
+
+	/**
+	 * Coverage for get_past_events method.
+	 *
+	 * @covers ::get_past_events
+	 * @covers ::order_past_events
+	 * @covers ::get_events_list
+	 * @covers ::pre_get_posts
+	 *
+	 * @return void
+	 */
+	public function test_get_past_events(): void {
+		$instance = Query::get_instance();
+		$response = $instance->get_past_events();
+
+		$this->assertEmpty( $response->posts, 'Failed to assert that posts array is empty.' );
+		$this->assertSame( 5, $response->query[ 'posts_per_page' ], 'Failed to assert post per page limit.' );
+
+		$post  = $this->mock->post( array( 'post_type' => 'gp_event' ) )->get();
+		$event = new Event( $post->ID );
+		$date  = new \DateTime( 'yesterday' );
+
+		$params = array(
+			'datetime_start' => $date->modify( '-1 day' )->format( 'Y-m-d H:i:s' ),
+			'datetime_end'   => $date->format( 'Y-m-d H:i:s' ),
+			'timezone'       => 'America/New_York',
+		);
+
+		$event->save_datetimes( $params );
+
+		$response = $instance->get_past_events( 1 );
+
+		$this->assertSame( $response->posts[0], $post->ID, 'Failed to assert that event ID is in array.' );
+		$this->assertSame( 1, $response->query[ 'posts_per_page' ], 'Failed to assert post per page limit.' );
+		$this->assertSame( 'past', $response->query[ 'gp_events_query' ], 'Failed to assert query is past.' );
+		$this->assertSame( 'gp_event', $response->query[ 'post_type' ], 'Failed to assert post type is gp_event.' );
 	}
 
 }
