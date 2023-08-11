ed32d7988451ba8bd9bbbbaa2ca9d0a7e56429ee
Mike Auteri <mauteri@gmail.com>
2023-08-11 19:35:49 -0400

    Fix small bugs on blocks.

--- build/blocks/attendance-list/index.asset.php
+++ build/blocks/attendance-list/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '180001d54ae314572114');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5ae251c326d3259b2aa2');
