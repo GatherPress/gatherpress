b5f18f9d9a3644804646aa764d2cf8dbd7b0abd8
Mike Auteri <mauteri@gmail.com>
2023-08-26 21:59:05 -0400

    Remove multiple limits on event blocks.

--- build/blocks/event-date/index.asset.php
+++ build/blocks/event-date/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '8481111f25432fa5bc00');
+<?php return array('dependencies' => array('moment', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '70e4970524b45ab6d41a');
--- build/blocks/online-event/index.asset.php
+++ build/blocks/online-event/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c8c56a42204077f94c14');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '324cc9630ed8269df6fb');
