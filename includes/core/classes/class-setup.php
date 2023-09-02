0c7def6c3379fa370ca26d6162b8584a3c171adf
Mike Auteri <mauteri@gmail.com>
2023-09-02 15:06:35 -0400

    Refactor setup class methods and hooks

--- includes/core/classes/class-setup.php
+++ includes/core/classes/class-setup.php
@@ -1,30 +1,40 @@
 <?php
 /**
- * Class is responsible for executing plugin setups.
+ * Manages plugin setup and initialization.
  *
- * @package GatherPress
- * @subpackage Core
+ * This class handles various aspects of plugin setup, including registering custom post types and taxonomies,
+ * creating custom database tables, and setting up plugin hooks.
+ *
+ * @package GatherPress\Core
  * @since 1.0.0
  */
 
 namespace GatherPress\Core;
 
+use Exception;
 use GatherPress\Core\Traits\Singleton;
 use WP_CLI;
+use WP_Post;
 
-if ( ! defined( 'ABSPATH' ) ) { // @codeCoverageIgnore
-	exit; // @codeCoverageIgnore
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // @codeCoverageIgnore Prevent direct access.
 }
 
 /**
  * Class Setup.
+ *
+ * Manages plugin setup and initialization.
+ *
+ * @since 1.0.0
  */
 class Setup {
 
 	use Singleton;
 
 	/**
-	 * Setup constructor.
+	 * Constructor for the Setup class.
+	 *
+	 * Initializes and sets up various components of the plugin.
 	 */
 	protected function __construct() {
 		$this->instantiate_classes();
@@ -32,27 +42,38 @@ class Setup {
 	}
 
 	/**
-	 * Instantiate singletons.
+	 * Instantiate singleton classes and set up WP-CLI command.
+	 *
+	 * This method initializes various singleton classes used by the plugin
+	 * and adds a WP-CLI command if WP_CLI is defined. It may throw an Exception
+	 * if there are issues instantiating the classes.
+	 *
+	 * @return void
+	 * @throws Exception If there are issues instantiating singleton classes.
+	 * @since 1.0.0
 	 */
-	protected function instantiate_classes() {
+	protected function instantiate_classes(): void {
 		Assets::get_instance();
 		Block::get_instance();
 		Query::get_instance();
 		Rest_Api::get_instance();
 		Settings::get_instance();
 		Venue::get_instance();
-		// @todo move these classes to a `buddypress` directory in plugin.
-		// BuddyPress::get_instance();
-		// Email::get_instance();
+
 		if ( defined( 'WP_CLI' ) && WP_CLI ) {
 			WP_CLI::add_command( 'gatherpress', Cli::class );
 		}
 	}
 
 	/**
-	 * Setup hooks.
+	 * Set up hooks for various purposes.
+	 *
+	 * This method adds hooks for different purposes as needed.
+	 *
+	 * @return void
+	 * @since 1.0.0
 	 */
-	protected function setup_hooks() {
+	protected function setup_hooks(): void {
 		register_activation_hook( GATHERPRESS_CORE_FILE, array( $this, 'activate_gatherpress_plugin' ) );
 		register_deactivation_hook( GATHERPRESS_CORE_FILE, array( $this, 'deactivate_gatherpress_plugin' ) );
 
@@ -66,7 +87,7 @@ class Setup {
 		);
 		add_action( 'init', array( $this, 'maybe_flush_gatherpress_rewrite_rules' ) );
 
-		add_filter( 'block_categories_all', array( $this, 'block_category' ) );
+		add_filter( 'block_categories_all', array( $this, 'register_gatherpress_block_category' ) );
 		add_filter( 'wpmu_drop_tables', array( $this, 'on_site_delete' ) );
 		add_filter(
 			sprintf( 'manage_%s_posts_columns', Event::POST_TYPE ),
@@ -78,7 +99,7 @@ class Setup {
 		);
 		add_filter( 'get_the_date', array( $this, 'get_the_event_date' ) );
 		add_filter( 'the_time', array( $this, 'get_the_event_date' ) );
-		add_filter( 'body_class', array( $this, 'body_class' ) );
+		add_filter( 'body_class', array( $this, 'add_gatherpress_body_classes' ) );
 		add_filter( 'display_post_states', array( $this, 'set_event_archive_labels' ), 10, 2 );
 		add_filter(
 			sprintf(
@@ -99,13 +120,17 @@ class Setup {
 	}
 
 	/**
-	 * Add links to the plugin action links.
+	 * Add custom links to the plugin action links in the WordPress plugins list.
 	 *
-	 * @param array $actions Array of links.
+	 * This method adds a 'Settings' link to the plugin's action links in the WordPress plugins list.
 	 *
-	 * @return array
+	 * @param array $actions An array of existing action links.
+	 *
+	 * @return array An updated array of action links, including the 'Settings' link.
+	 *
+	 * @since 1.0.0
 	 */
-	public function filter_plugin_action_links( array $actions ) {
+	public function filter_plugin_action_links( array $actions ): array {
 		return array_merge(
 			array(
 				'settings' => '<a href="' . esc_url( admin_url( 'edit.php?post_type=gp_event&page=gp_general' ) ) . '">' . esc_html__( 'Settings', 'gatherpress' ) . '</a>',
@@ -115,11 +140,16 @@ class Setup {
 	}
 
 	/**
-	 * Activate GatherPress plugin.
+	 * Activate the GatherPress plugin.
+	 *
+	 * This method performs activation tasks for the GatherPress plugin, such as renaming blocks and tables,
+	 * creating custom tables, and setting a flag to flush rewrite rules if necessary.
+	 *
+	 * @since 1.0.0
 	 *
 	 * @return void
 	 */
-	public function activate_gatherpress_plugin() {
+	public function activate_gatherpress_plugin(): void {
 		$this->maybe_rename_blocks();
 		$this->maybe_rename_table();
 		$this->maybe_create_custom_table();
@@ -130,19 +160,30 @@ class Setup {
 	}
 
 	/**
-	 * Activate GatherPress plugin.
+	 * Deactivate the GatherPress plugin.
+	 *
+	 * This method is called when deactivating the GatherPress plugin. It flushes the rewrite rules to ensure
+	 * proper functionality.
+	 *
+	 * @since 1.0.0
 	 *
 	 * @return void
 	 */
-	public function deactivate_gatherpress_plugin() {
+	public function deactivate_gatherpress_plugin(): void {
 		flush_rewrite_rules();
 	}
 
 	/**
-	 * Flush rewrite rules if the previously added flag exists,
-	 * and then remove the flag.
+	 * Flush GatherPress rewrite rules if the previously added flag exists and then remove the flag.
+	 *
+	 * This method checks if the 'gatherpress_flush_rewrite_rules_flag' option exists. If it does, it flushes
+	 * the rewrite rules to ensure they are up to date and removes the flag afterward.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
 	 */
-	public function maybe_flush_gatherpress_rewrite_rules() {
+	public function maybe_flush_gatherpress_rewrite_rules(): void {
 		if ( get_option( 'gatherpress_flush_rewrite_rules_flag' ) ) {
 			flush_rewrite_rules();
 			delete_option( 'gatherpress_flush_rewrite_rules_flag' );
@@ -150,13 +191,18 @@ class Setup {
 	}
 
 	/**
-	 * AddAttendanceSelector.js body class for active theme.
+	 * Add GatherPress-specific body classes to the existing body classes.
 	 *
-	 * @param array $classes Body classes.
+	 * This method appends custom body classes, such as 'gp-enabled' and 'gp-theme-{theme-name}',
+	 * to the array of existing body classes.
 	 *
-	 * @return mixed
+	 * @since 1.0.0
+	 *
+	 * @param array $classes Existing body classes.
+	 *
+	 * @return array An updated array of body classes.
 	 */
-	public function body_class( $classes ) {
+	public function add_gatherpress_body_classes( array $classes ): array {
 		$classes[] = 'gp-enabled';
 		$classes[] = sprintf( 'gp-theme-%s', esc_attr( get_stylesheet() ) );
 
@@ -164,47 +210,59 @@ class Setup {
 	}
 
 	/**
-	 * Add GatherPress block category.
+	 * Register GatherPress block category.
 	 *
-	 * @param array $block_categories All the registered block categories.
+	 * This method registers the GatherPress block category and adds it to the array
+	 * of registered block categories.
 	 *
-	 * @return array
+	 * @since 1.0.0
+	 *
+	 * @param array $block_categories Array of registered block categories.
+	 *
+	 * @return array An updated array of block categories.
 	 */
-	public function block_category( $block_categories ) {
-		$gatherpress_category = array(
+	public function register_gatherpress_block_category( array $block_categories ): array {
+		$category = array(
 			'slug'  => 'gatherpress',
 			'title' => __( 'GatherPress', 'gatherpress' ),
 			'icon'  => 'nametag',
 		);
 
-		array_unshift( $block_categories, $gatherpress_category );
+		array_unshift( $block_categories, $category );
 
 		return $block_categories;
 	}
 
 	/**
-	 * Register the GatherPress post types and taxonomies.
+	 * Register GatherPress post types and taxonomies.
+	 *
+	 * This method is responsible for registering the GatherPress post types and taxonomies,
+	 * as well as adding the online event term. It initializes the necessary content structures
+	 * for the plugin.
 	 *
 	 * @since 1.0.0
 	 *
 	 * @return void
 	 */
-	public function register() {
+	public function register(): void {
 		$this->register_post_types();
 		$this->register_taxonomies();
 		$this->add_online_event_term();
 	}
 
 	/**
-	 * Register the GatherPress post types.
+	 * Register GatherPress post types.
 	 *
-	 * @todo cleanup registering so it's not so repeated.
+	 * This method is responsible for registering the GatherPress post types, including
+	 * 'Event' and 'Venue' post types. It sets up their labels, supports, and other parameters.
+	 *
+	 * @todo Consider refactoring to reduce repetition in post type registration.
 	 *
 	 * @since 1.0.0
 	 *
 	 * @return void
 	 */
-	public function register_post_types() {
+	public function register_post_types(): void {
 		register_post_type(
 			Event::POST_TYPE,
 			array(
@@ -323,15 +381,18 @@ class Setup {
 	}
 
 	/**
-	 * Register the GatherPress taxonomies.
+	 * Register GatherPress taxonomies.
 	 *
-	 * @todo cleanup registering so it's not so repeated.
+	 * This method is responsible for registering the GatherPress taxonomies, including
+	 * the 'Topics' taxonomy. It sets up their labels, hierarchical structure, and other parameters.
+	 *
+	 * @todo Consider refactoring to reduce repetition in taxonomy registration.
 	 *
 	 * @since 1.0.0
 	 *
 	 * @return void
 	 */
-	public function register_taxonomies() {
+	public function register_taxonomies(): void {
 		register_taxonomy(
 			Event::TAXONOMY,
 			Event::POST_TYPE,
@@ -378,7 +439,10 @@ class Setup {
 	}
 
 	/**
-	 * Add online event term.
+	 * Add the 'Online event' term to the venue taxonomy.
+	 *
+	 * This method adds the 'Online event' term to the venue taxonomy if it does not exist,
+	 * or updates it if it already exists. This term is used to categorize online events.
 	 *
 	 * @since 1.0.0
 	 *
@@ -410,13 +474,17 @@ class Setup {
 	}
 
 	/**
-	 * Delete custom table on site deletion.
+	 * Delete custom tables on site deletion.
+	 *
+	 * This method is called when a site is deleted, and it allows the plugin to specify
+	 * which custom tables associated with the plugin should be deleted. It returns an
+	 * updated array of table names to be dropped during site deletion.
 	 *
 	 * @since 1.0.0
 	 *
-	 * @param array $tables Array of names of the site tables to be dropped.
+	 * @param array $tables An array of names of the site tables to be dropped.
 	 *
-	 * @return array
+	 * @return array An updated array of table names to be deleted during site deletion.
 	 */
 	public function on_site_delete( array $tables ): array {
 		global $wpdb;
@@ -428,13 +496,18 @@ class Setup {
 	}
 
 	/**
-	 * Delete event record from custom table when event is deleted.
+	 * Delete event record from custom table when an event is deleted.
+	 *
+	 * This method is called when an event post is deleted, and it ensures that the corresponding
+	 * record in the custom table associated with the event is also deleted.
 	 *
 	 * @since 1.0.0
 	 *
 	 * @param int $post_id An event post ID.
+	 *
+	 * @return void
 	 */
-	public function delete_event( int $post_id ) {
+	public function delete_event( int $post_id ): void {
 		global $wpdb;
 
 		if ( Event::POST_TYPE !== get_post_type( $post_id ) ) {
@@ -452,9 +525,14 @@ class Setup {
 	}
 
 	/**
-	 * Rename attendees table to rsvps.
+	 * Rename the attendees table to rsvps.
 	 *
-	 * @todo remove this code, just temporary to address a breaking change.
+	 * @todo Remove this code with 1.0.0; it's temporary to address a breaking change.
+	 *
+	 * This method renames the attendees table to rsvps, but it's intended as a temporary solution
+	 * to handle a breaking change. Evaluate whether this code can be removed in the future.
+	 *
+	 * @since 1.0.0
 	 *
 	 * @return void
 	 */
@@ -468,9 +546,15 @@ class Setup {
 	}
 
 	/**
-	 * Rename attendance blocks to rsvp.
+	 * Rename attendance blocks to RSVP blocks.
 	 *
-	 * @todo remove this code, just temporary to address a breaking change.
+	 * @todo Remove this code with 1.0.0; it's temporary to address a breaking change.
+	 *
+	 * This method scans and updates content for all posts of specified types, replacing
+	 * occurrences of attendance-related blocks with RSVP-related blocks. It's recommended
+	 * to review and potentially remove this code once the transition is complete.
+	 *
+	 * @since 1.0.0
 	 *
 	 * @return void
 	 */
@@ -515,7 +599,11 @@ class Setup {
 	}
 
 	/**
-	 * Maybe create custom table if doesn't exist for main site or current site in network.
+	 * Create a custom table if it doesn't exist for the main site or the current site in a network.
+	 *
+	 * This method checks whether the custom database tables required for the plugin exist
+	 * and creates them if they don't. It handles both the main site and, in a multisite network,
+	 * the current site.
 	 *
 	 * @since 1.0.0
 	 *
@@ -534,11 +622,16 @@ class Setup {
 	}
 
 	/**
-	 * Create custom event table.
+	 * Create custom database tables for GatherPress events and RSVPs.
+	 *
+	 * This method creates custom database tables for storing GatherPress event data and RSVP information.
+	 * It ensures that the required tables are set up with the appropriate schema.
 	 *
 	 * @since 1.0.0
+	 *
+	 * @return void
 	 */
-	protected function create_tables() {
+	protected function create_tables(): void {
 		global $wpdb;
 
 		$sql             = array();
@@ -577,31 +670,37 @@ class Setup {
 	}
 
 	/**
-	 * Populate custom columns for Event post type.
+	 * Populate custom columns for Event post type in the admin dashboard.
+	 *
+	 * This method is used to display custom columns for Event post types in the WordPress admin dashboard.
+	 * It provides additional information for each event, such as its datetime.
 	 *
 	 * @since 1.0.0
 	 *
 	 * @param string $column  The name of the column to display.
 	 * @param int    $post_id The current post ID.
+	 *
+	 * @return void
 	 */
-	public function custom_columns( string $column, int $post_id ) {
-		$event = new Event( $post_id );
+	public function custom_columns( string $column, int $post_id ): void {
+		if ( 'datetime' === $column ) {
+			$event = new Event( $post_id );
 
-		switch ( $column ) {
-			case 'datetime':
-				echo esc_html( $event->get_display_datetime() );
-				break;
+			echo esc_html( $event->get_display_datetime() );
 		}
 	}
 
 	/**
-	 * Set custom columns for Event post type.
+	 * Set custom columns for Event post type in the admin dashboard.
+	 *
+	 * This method is used to define custom columns for Event post types in the WordPress admin dashboard.
+	 * It adds an additional column for displaying event date and time.
 	 *
 	 * @since 1.0.0
 	 *
 	 * @param array $columns An associative array of column headings.
 	 *
-	 * @return array
+	 * @return array An updated array of column headings, including the custom columns.
 	 */
 	public function set_custom_columns( array $columns ): array {
 		$placement = 2;
@@ -613,37 +712,47 @@ class Setup {
 	}
 
 	/**
-	 * Make custom columns sortable for Event post type.
+	 * Make custom columns sortable for Event post type in the admin dashboard.
+	 *
+	 * This method allows the custom columns, including the 'Event date & time' column,
+	 * to be sortable in the WordPress admin dashboard for Event post types.
 	 *
 	 * @since 1.0.0
 	 *
 	 * @param array $columns An array of sortable columns.
 	 *
-	 * @return array
+	 * @return array An updated array of sortable columns.
 	 */
 	public function sortable_columns( array $columns ): array {
+		// Add 'datetime' as a sortable column.
 		$columns['datetime'] = 'datetime';
 
 		return $columns;
 	}
 
 	/**
-	 * Returns the event date instead of publish date for events.
+	 * Returns the event date instead of the publish date for events.
+	 *
+	 * This method retrieves the event date instead of the publish date for events
+	 * based on the plugin settings. It checks if the event date should be used instead
+	 * of the publish date and returns the formatted event date accordingly.
 	 *
 	 * @since 1.0.0
 	 *
 	 * @param string $the_date The formatted date.
 	 *
-	 * @return string
+	 * @return string The updated date string, representing the event date.
 	 */
 	public function get_the_event_date( $the_date ): string {
 		$settings       = Settings::get_instance();
 		$use_event_date = $settings->get_value( 'gp_general', 'general', 'post_or_event_date' );
 
+		// Check if the post is of the 'Event' post type and if event date should be used.
 		if ( Event::POST_TYPE !== get_post_type() || 1 !== intval( $use_event_date ) ) {
 			return $the_date;
 		}
 
+		// Get the event date and return it as the formatted date.
 		$event = new Event( get_the_ID() );
 
 		return $event->get_display_datetime();
@@ -652,12 +761,19 @@ class Setup {
 	/**
 	 * Add Upcoming and Past Events display states to assigned pages.
 	 *
-	 * @param array    $post_states An array of post display states.
-	 * @param \WP_Post $post        The current post object.
+	 * This method adds custom display states to assigned pages for "Upcoming Events" and "Past Events"
+	 * based on the plugin settings. It checks if the current post object corresponds to any of the assigned
+	 * pages and adds display states accordingly.
 	 *
-	 * @return array
+	 * @since 1.0.0
+	 *
+	 * @param array   $post_states An array of post display states.
+	 * @param WP_Post $post        The current post object.
+	 *
+	 * @return array An updated array of post display states with custom labels if applicable.
 	 */
-	public function set_event_archive_labels( array $post_states, \WP_Post $post ) {
+	public function set_event_archive_labels( array $post_states, WP_Post $post ): array {
+		// Retrieve plugin general settings.
 		$general = get_option( Utility::prefix_key( 'general' ) );
 		$pages   = $general['pages'] ?? '';
 
@@ -665,11 +781,13 @@ class Setup {
 			return $post_states;
 		}
 
+		// Define archive pages for "Upcoming Events" and "Past Events".
 		$archive_pages = array(
 			'past_events'     => json_decode( $pages['past_events'] ),
 			'upcoming_events' => json_decode( $pages['upcoming_events'] ),
 		);
 
+		// Check if the current post corresponds to any assigned archive page and add display states.
 		foreach ( $archive_pages as $key => $value ) {
 			if ( ! empty( $value ) && is_array( $value ) ) {
 				$page = $value[0];
--- includes/core/classes/class-venue.php
+++ includes/core/classes/class-venue.php
@@ -1,9 +1,11 @@
 <?php
 /**
- * Class is responsible for instances of venues.
+ * Class responsible for managing Venue instances.
  *
- * @package GatherPress
- * @subpackage Core
+ * This class handles the management of Venue instances, including actions related to Venue post types,
+ * Venue taxonomies, and associated operations such as adding Venue terms, updating term slugs, and more.
+ *
+ * @package GatherPress\Core
  * @since 1.0.0
  */
 
@@ -12,32 +14,49 @@ namespace GatherPress\Core;
 use GatherPress\Core\Traits\Singleton;
 use WP_Post;
 
-if ( ! defined( 'ABSPATH' ) ) { // @codeCoverageIgnore
-	exit; // @codeCoverageIgnore
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // @codeCoverageIgnore Prevent direct access.
 }
 
 /**
  * Class Venue.
+ *
+ * Handles the management of Venue instances, including Venue post types and taxonomies.
+ *
+ * @since 1.0.0
  */
 class Venue {
 
 	use Singleton;
 
 	/**
-	 * Constants.
+	 * Constants for Venue Post Type and Taxonomy.
+	 *
+	 * Defines constants for the Venue post type and its associated taxonomy.
+	 *
+	 * @since 1.0.0
 	 */
 	const POST_TYPE = 'gp_venue';
 	const TAXONOMY  = '_gp_venue';
 
 	/**
-	 * Venue constructor.
+	 * Class constructor.
+	 *
+	 * This method initializes the object and sets up necessary hooks.
+	 *
+	 * @since 1.0.0
 	 */
 	public function __construct() {
 		$this->setup_hooks();
 	}
 
 	/**
-	 * Setup hooks.
+	 * Set up hooks for various purposes.
+	 *
+	 * This method adds hooks for different purposes as needed.
+	 *
+	 * @return void
+	 * @since 1.0.0
 	 */
 	protected function setup_hooks(): void {
 		add_action(
@@ -51,10 +70,15 @@ class Venue {
 	}
 
 	/**
-	 * Add venue term when venue post type first saves.
+	 * Add a venue term when a venue post type is first saved.
 	 *
-	 * @param int     $post_id Post ID.
-	 * @param WP_Post $post    Post object.
+	 * This method is responsible for automatically adding a term to the venue taxonomy
+	 * when a new venue post is created and published.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int     $post_id Post ID of the venue post.
+	 * @param WP_Post $post    The venue post object.
 	 * @param bool    $update  Whether this is an existing post being updated.
 	 *
 	 * @return void
@@ -86,31 +110,45 @@ class Venue {
 	}
 
 	/**
-	 * If the slug of venue post changes, update slug for corresponding venue term.
+	 * Update the slug of the corresponding venue term if the venue post's slug changes.
 	 *
-	 * @param int     $post_id     Post ID.
-	 * @param WP_Post $post_after  Post object after save.
-	 * @param WP_Post $post_before Post object before save.
+	 * This method is triggered when a venue post is updated and checks if the slug of the
+	 * venue post has changed. If it has changed, it updates the corresponding venue term's
+	 * slug to match the new venue post slug.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int     $post_id     Post ID of the venue post.
+	 * @param WP_Post $post_after  Post object after the save operation.
+	 * @param WP_Post $post_before Post object before the save operation.
 	 *
 	 * @return void
 	 */
 	public function maybe_update_term_slug( int $post_id, WP_Post $post_after, WP_Post $post_before ): void {
+		// Check if the post type is Venue.
 		if ( self::POST_TYPE !== get_post_type( $post_id ) ) {
 			return;
 		}
 
+		// Only proceed if the venue post is being published.
 		if ( 'publish' !== $post_after->post_status ) {
 			return;
 		}
 
+		// Check if the post slug or title has changed.
 		if (
 			$post_before->post_name !== $post_after->post_name ||
 			$post_before->post_title !== $post_after->post_title
 		) {
+			// Calculate the old and new term slugs.
 			$old_term_slug = $this->get_venue_term_slug( $post_before->post_name );
 			$new_term_slug = $this->get_venue_term_slug( $post_after->post_name );
-			$title         = html_entity_decode( get_the_title( $post_id ) );
-			$term          = term_exists( $old_term_slug, self::TAXONOMY );
+
+			// Decode the title to ensure special characters are handled correctly.
+			$title = html_entity_decode( get_the_title( $post_id ) );
+
+			// Check if the old term exists, and if not, insert the new term.
+			$term = term_exists( $old_term_slug, self::TAXONOMY );
 
 			if ( empty( $term ) ) {
 				wp_insert_term(
@@ -121,6 +159,7 @@ class Venue {
 					)
 				);
 			} else {
+				// Update the existing term with the new name and slug.
 				wp_update_term(
 					$term['term_id'],
 					self::TAXONOMY,
@@ -134,18 +173,31 @@ class Venue {
 	}
 
 	/**
-	 * Delete a venue term when a Venue is deleted.
+	 * Delete the corresponding venue term when a Venue post is deleted.
 	 *
-	 * @param int $post_id Post ID of venue.
+	 * This method is triggered when a Venue post is deleted. It checks if the deleted post
+	 * is of the Venue post type and, if so, deletes the corresponding venue term associated
+	 * with the post.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int $post_id Post ID of the Venue post being deleted.
 	 *
 	 * @return void
 	 */
 	public function delete_venue_term( int $post_id ): void {
+		// Check if the post type is Venue.
 		if ( get_post_type( $post_id ) === self::POST_TYPE ) {
-			$post      = get_post( $post_id );
-			$term_slug = $this->get_venue_term_slug( $post->post_name );
-			$term      = get_term_by( 'slug', $term_slug, self::TAXONOMY );
+			// Retrieve the post object.
+			$post = get_post( $post_id );
 
+			// Generate the term slug associated with the Venue post.
+			$term_slug = $this->get_venue_term_slug( $post->post_name );
+
+			// Get the term by slug.
+			$term = get_term_by( 'slug', $term_slug, self::TAXONOMY );
+
+			// Check if the term exists and delete it.
 			if ( is_a( $term, '\WP_Term' ) ) {
 				wp_delete_term( $term->term_id, self::TAXONOMY );
 			}
@@ -153,34 +205,55 @@ class Venue {
 	}
 
 	/**
-	 * Term slug for venue taxonomy which incorporates post slug of venue CPT.
+	 * Generate a term slug for the Venue taxonomy based on the post slug of a Venue post.
 	 *
-	 * @param string $post_name Post name of venue.
+	 * This method generates a unique term slug for the Venue taxonomy by incorporating
+	 * the post slug of a Venue post. It is used to create and identify the corresponding
+	 * venue term associated with a Venue post.
 	 *
-	 * @return string
+	 * @since 1.0.0
+	 *
+	 * @param string $post_name Post name (slug) of the Venue post.
+	 *
+	 * @return string The generated term slug.
 	 */
 	public function get_venue_term_slug( string $post_name ): string {
+		// Generate the term slug by prefixing it with an underscore.
 		return sprintf( '_%s', $post_name );
 	}
 
 	/**
-	 * Get the Venue CPT from Venue taxonomy slug.
+	 * Retrieve the Venue Custom Post Type (CPT) from a Venue taxonomy slug.
 	 *
-	 * @param string $slug Slug of venue taxonomy to retrieve venue post object.
+	 * This method retrieves the Venue Custom Post Type (CPT) associated with a given
+	 * Venue taxonomy slug. It allows you to obtain the Venue post object based on the
+	 * taxonomy slug used for venues.
 	 *
-	 * @return null|WP_Post
+	 * @since 1.0.0
+	 *
+	 * @param string $slug Slug of the Venue taxonomy to retrieve the Venue post.
+	 *
+	 * @return null|WP_Post The Venue post object if found; otherwise, null.
 	 */
 	public function get_venue_post_from_term_slug( string $slug ): ?WP_Post {
+		// Remove any leading underscores from the slug and retrieve the corresponding Venue post.
 		return get_page_by_path( ltrim( $slug, '_' ), OBJECT, self::POST_TYPE );
 	}
 
 	/**
-	 * Get venue information from meta data.
+	 * Retrieve venue information from meta data.
 	 *
-	 * @param int    $post_id   the post ID.
-	 * @param string $post_type the post type.
+	 * This method retrieves and assembles venue-related information from meta data
+	 * associated with a given post. The returned array includes details such as the
+	 * venue's name, whether it's an online event term, an online event link, and any
+	 * additional venue information stored as JSON in post meta.
 	 *
-	 * @return array
+	 * @since 1.0.0
+	 *
+	 * @param int    $post_id   The post ID for which to retrieve venue information.
+	 * @param string $post_type The post type of the provided post ID.
+	 *
+	 * @return array An array containing venue-related information.
 	 */
 	public function get_venue_meta( int $post_id, string $post_type ): array {
 		$venue_post = null;
