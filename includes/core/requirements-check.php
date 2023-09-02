abc0af46e8459f3cbeb7eda0b03d1334ddd1d8f0
Mike Auteri <mauteri@gmail.com>
2023-09-02 12:04:56 -0400

    Check system requirements before loading plugin

--- includes/core/requirements-check.php
+++ includes/core/requirements-check.php
@@ -1,15 +1,16 @@
 <?php
 /**
- * Checks requirements before loading plugin.
+ * GatherPress Plugin Requirements Check.
  *
- * @package GatherPress
- * @subpackage Core
+ * This file checks the system requirements before loading the GatherPress plugin.
+ *
+ * @package GatherPress\Core
  * @since 1.0.0
  */
 
 $gatherpress_preflight_return = true;
 
-// Check version of PHP before loading plugin.
+// Check the PHP version to ensure compatibility with the plugin.
 if ( version_compare( PHP_VERSION_ID, GATHERPRESS_MINIMUM_PHP_VERSION, '<' ) ) {
 	add_action(
 		'admin_notices',
@@ -20,7 +21,7 @@ if ( version_compare( PHP_VERSION_ID, GATHERPRESS_MINIMUM_PHP_VERSION, '<' ) ) {
 					<?php
 					echo sprintf(
 						/* translators: %1$s: minimum PHP version, %2$s current PHP version. */
-						esc_html__( 'GatherPress requires PHP Version %1$s or greater. You are currently running PHP %2$s. Please upgrade.', 'gatherpress' ),
+						esc_html__( 'GatherPress requires PHP %1$s or higher. Your current PHP version is %2$s. Please upgrade.', 'gatherpress' ),
 						esc_html( GATHERPRESS_MINIMUM_PHP_VERSION ),
 						esc_html( phpversion() )
 					);
