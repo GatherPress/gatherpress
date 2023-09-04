c50859074fe1c812adad3f2e6e25859c24231654
Jeffrey Marx <jeffrey_marx@yahoo.com>
2023-09-04 07:44:06 -0400

    Get those spaces right

--- tests/includes/core/classes/class-test-query.php
+++ tests/includes/core/classes/class-test-query.php
@@ -131,21 +131,21 @@ class Test_Query extends Base {
 
 		$instance = Query::get_instance();
 		
-		$this->mock->user(false, 'admin');
-		$response = $instance->adjust_admin_event_sorting(array());
+		$this->mock->user( false, 'admin' );
+		$response = $instance->adjust_admin_event_sorting( array() );
 		$this->assertEmpty($response, 'The array is not empty.');
 
-		$this->mock->user(true, 'admin');
+		$this->mock->user( true, 'admin' );
 
-		//set 'orderby' admin query to 'datetime'
+		//Set 'orderby' admin query to 'datetime'.
 		global $wp_query;
-		$wp_query->set('orderby', 'datetime');
+		$wp_query->set( 'orderby', 'datetime' );
 
 		//Run function with empty array passed as 'pieces' argument.
-		$response = $instance->adjust_admin_event_sorting(array());
+		$response = $instance->adjust_admin_event_sorting( array() );
 
 	    // Assert that an array was generated from the adjustsql argument. todo: make this test more meaningful.
-		$this->assertNotEmpty($response, 'The array is empty.');
+		$this->assertNotEmpty( $response, 'The array is empty.' );
 	}
 
 }
