c8aace36a05f8c9c045a389745fc259f652290a6
Paul Barthmaier <pbrocks@users.noreply.github.com>
2023-09-06 00:09:34 -0400

    Update dismiss-notification.php

--- includes/templates/admin/settings/dismiss-notification.php
+++ includes/templates/admin/settings/dismiss-notification.php
@@ -11,22 +11,19 @@
  * @param string $page      The current settings page.
  */
 
-wp_nonce_field( 'suppress_gp_membership_notification', 'suppress_gp_membership' );
+$nonce = wp_nonce_url( add_query_arg( 'action', 'suppress_gp_membership_notification' ), 'clear-notification' );
 
 ?>
 	<div class="notice notice-warning is-dismissible" id="gp-membership">
 		<p>
 			<?php
 				/* translators: %s: search term */
-				__(
-					printf(
-						'To hide GatherPress functions optimally, we recommend enabling user registration. You can do so by <a href=%s>enabling user registration here</a>. <a href=%s><button>Dismiss forever</button></a>',
-						esc_url(
-							admin_url( 'options-general.php#users_can_register' )
-						),
-						wp_nonce_url( add_query_arg( 'action', 'suppress_gp_membership_notification' ), 'clear-notification' ),
-						'gatherpress'
-					)
+				printf(
+					'%s <a href=%s>%s</a>. <a href=%s><button>Dismiss forever</button></a>',
+					esc_html__( 'To ensure GatherPress functions optimally, we recommend enabling user registration. You can do so by', 'gatherpress' ),
+					esc_url( admin_url( 'options-general.php#users_can_register' ) ),
+					esc_html__( 'enabling user registration here', 'gatherpress' ),
+					esc_url( $nonce )
 				);
 				?>
 		</p>
