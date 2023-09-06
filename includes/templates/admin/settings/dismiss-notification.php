949fecc4ce3183a29ff4a8551272b53ea7d39496
Paul Barthmaier <pbrocks@users.noreply.github.com>
2023-09-06 00:14:04 -0400

    Update dismiss-notification.php

--- includes/templates/admin/settings/dismiss-notification.php
+++ includes/templates/admin/settings/dismiss-notification.php
@@ -1,17 +1,11 @@
 <?php
 /**
- * Template for GatherPress settings pages.
- *
- * This template is used to display and manage settings for the GatherPress plugin.
+ * Admin Notice for open membership check.
  *
  * @package GatherPress\Core
  * @since 1.0.0
  *
- * @param array  $sub_pages An array of sub-pages and their corresponding values.
- * @param string $page      The current settings page.
  */
-
-$nonce = wp_nonce_url( add_query_arg( 'action', 'suppress_gp_membership_notification' ), 'clear-notification' );
 
 ?>
 	<div class="notice notice-warning is-dismissible" id="gp-membership">
@@ -23,7 +17,7 @@ $nonce = wp_nonce_url( add_query_arg( 'action', 'suppress_gp_membership_notifica
 					esc_html__( 'To ensure GatherPress functions optimally, we recommend enabling user registration. You can do so by', 'gatherpress' ),
 					esc_url( admin_url( 'options-general.php#users_can_register' ) ),
 					esc_html__( 'enabling user registration here', 'gatherpress' ),
-					esc_url( $nonce )
+					esc_url( wp_nonce_url( add_query_arg( 'action', 'suppress_gp_membership_notification' ), 'clear-notification' ) )
 				);
 				?>
 		</p>
