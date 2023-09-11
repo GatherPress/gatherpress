bebc03fbdc00c4e5ed9be4c1c1c7aad1126fd7b7
Mike Auteri <mauteri@gmail.com>
2023-09-10 22:33:40 -0400

    Test plugin version includes credits and package JSON versions

--- tests/class-test-gatherpress.php
+++ tests/class-test-gatherpress.php
@@ -22,11 +22,18 @@ class Test_GatherPress extends Base {
 	 * @return void
 	 */
 	public function test_plugin_version(): void {
+		$credits      = include sprintf( '%s/includes/data/credits/latest.php', GATHERPRESS_CORE_PATH );
 		$package_json = json_decode(
 			file_get_contents( // phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
 				sprintf( '%s/package.json', GATHERPRESS_CORE_PATH )
 			),
 			true
+		);
+
+		$this->assertSame(
+			$credits['version'],
+			GATHERPRESS_VERSION,
+			'Failed to assert version in gatherpress.php matches version in latest.php.'
 		);
 
 		$this->assertSame(
