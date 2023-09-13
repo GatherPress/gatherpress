f4a5fe277a62a17f7229eff113e751b41747c391
Mike Auteri <mauteri@gmail.com>
2023-09-13 00:08:17 -0400

    Sort contributors alphabetically in CLI credits list

--- includes/data/credits/latest.php
+++ includes/data/credits/latest.php
@@ -88,31 +88,18 @@
   array (
     0 => 
     array (
-      'id' => 11704207,
-      'name' => 'hauvong',
-      'link' => 'https://profiles.wordpress.org/author/hauvong/',
-      'slug' => 'hauvong',
+      'id' => 14040242,
+      'name' => 'Cameron Barrett',
+      'link' => 'https://profiles.wordpress.org/author/cameronbarrett/',
+      'slug' => 'cameronbarrett',
       'avatar_urls' => 
       array (
-        24 => '//www.gravatar.com/avatar/b3bb6b6a8a28be5c130db072cd4b553e?s=24&#038;r=g&#038;d=mm',
-        48 => '//www.gravatar.com/avatar/b3bb6b6a8a28be5c130db072cd4b553e?s=48&#038;r=g&#038;d=mm',
-        96 => '//www.gravatar.com/avatar/b3bb6b6a8a28be5c130db072cd4b553e?s=96&#038;r=g&#038;d=mm',
+        24 => '//www.gravatar.com/avatar/1e12bb14c1bf783f1560e082e55b0481?s=24&#038;r=g&#038;d=mm',
+        48 => '//www.gravatar.com/avatar/1e12bb14c1bf783f1560e082e55b0481?s=48&#038;r=g&#038;d=mm',
+        96 => '//www.gravatar.com/avatar/1e12bb14c1bf783f1560e082e55b0481?s=96&#038;r=g&#038;d=mm',
       ),
     ),
     1 => 
-    array (
-      'id' => 15490728,
-      'name' => 'Jason Lawton',
-      'link' => 'https://profiles.wordpress.org/author/phoopee3/',
-      'slug' => 'phoopee3',
-      'avatar_urls' => 
-      array (
-        24 => '//www.gravatar.com/avatar/ec5ec8357fa89bb2671bf1ab61b2546b?s=24&#038;r=g&#038;d=mm',
-        48 => '//www.gravatar.com/avatar/ec5ec8357fa89bb2671bf1ab61b2546b?s=48&#038;r=g&#038;d=mm',
-        96 => '//www.gravatar.com/avatar/ec5ec8357fa89bb2671bf1ab61b2546b?s=96&#038;r=g&#038;d=mm',
-      ),
-    ),
-    2 => 
     array (
       'id' => 2451432,
       'name' => 'Courtney Robertson',
@@ -125,7 +112,7 @@
         96 => '//www.gravatar.com/avatar/03ff681abf60368b6ce5f5edcf9be310?s=96&#038;r=g&#038;d=mm',
       ),
     ),
-    3 => 
+    2 => 
     array (
       'id' => 15706310,
       'name' => 'Dan Griffiths',
@@ -136,6 +123,19 @@
         24 => '//www.gravatar.com/avatar/d91c15fa32ff542598c931e939bf6040?s=24&#038;r=g&#038;d=mm',
         48 => '//www.gravatar.com/avatar/d91c15fa32ff542598c931e939bf6040?s=48&#038;r=g&#038;d=mm',
         96 => '//www.gravatar.com/avatar/d91c15fa32ff542598c931e939bf6040?s=96&#038;r=g&#038;d=mm',
+      ),
+    ),
+    3 => 
+    array (
+      'id' => 11704207,
+      'name' => 'hauvong',
+      'link' => 'https://profiles.wordpress.org/author/hauvong/',
+      'slug' => 'hauvong',
+      'avatar_urls' => 
+      array (
+        24 => '//www.gravatar.com/avatar/b3bb6b6a8a28be5c130db072cd4b553e?s=24&#038;r=g&#038;d=mm',
+        48 => '//www.gravatar.com/avatar/b3bb6b6a8a28be5c130db072cd4b553e?s=48&#038;r=g&#038;d=mm',
+        96 => '//www.gravatar.com/avatar/b3bb6b6a8a28be5c130db072cd4b553e?s=96&#038;r=g&#038;d=mm',
       ),
     ),
     4 => 
@@ -153,15 +153,15 @@
     ),
     5 => 
     array (
-      'id' => 14040242,
-      'name' => 'Cameron Barrett',
-      'link' => 'https://profiles.wordpress.org/author/cameronbarrett/',
-      'slug' => 'cameronbarrett',
+      'id' => 15490728,
+      'name' => 'Jason Lawton',
+      'link' => 'https://profiles.wordpress.org/author/phoopee3/',
+      'slug' => 'phoopee3',
       'avatar_urls' => 
       array (
-        24 => '//www.gravatar.com/avatar/1e12bb14c1bf783f1560e082e55b0481?s=24&#038;r=g&#038;d=mm',
-        48 => '//www.gravatar.com/avatar/1e12bb14c1bf783f1560e082e55b0481?s=48&#038;r=g&#038;d=mm',
-        96 => '//www.gravatar.com/avatar/1e12bb14c1bf783f1560e082e55b0481?s=96&#038;r=g&#038;d=mm',
+        24 => '//www.gravatar.com/avatar/ec5ec8357fa89bb2671bf1ab61b2546b?s=24&#038;r=g&#038;d=mm',
+        48 => '//www.gravatar.com/avatar/ec5ec8357fa89bb2671bf1ab61b2546b?s=48&#038;r=g&#038;d=mm',
+        96 => '//www.gravatar.com/avatar/ec5ec8357fa89bb2671bf1ab61b2546b?s=96&#038;r=g&#038;d=mm',
       ),
     ),
   ),
--- includes/templates/admin/settings/credits/index.php
+++ includes/templates/admin/settings/credits/index.php
@@ -58,17 +58,32 @@ if ( ! isset( $credits ) ) {
 		printf( esc_html__( 'Contributors to GatherPress %s', 'gatherpress' ), esc_html( $credits['version'] ) );
 		?>
 	</h3>
-	<ul class="gp-settings__credits-list">
+	<div class="gp-settings__credits-list">
 		<?php
-		foreach ( $credits['contributors'] as $gatherpress_contributor ) :
+		$gatherpress_contributor_count = count( $credits['contributors'] ?? array() );
+
+		foreach ( $credits['contributors'] as $gatherpress_key => $gatherpress_contributor ) :
+			$gatherpress_contributor_end = ', ';
+
+			if ( 2 === $gatherpress_contributor_count ) {
+				$gatherpress_contributor_end = ' and ';
+			} elseif ( $gatherpress_key + 2 === $gatherpress_contributor_count ) {
+				$gatherpress_contributor_end = ', and ';
+			}
+
+			if ( $gatherpress_key + 1 === $gatherpress_contributor_count ) {
+				$gatherpress_contributor_end = '.';
+			}
+
 			Utility::render_template(
 				sprintf( '%s/includes/templates/admin/settings/credits/contributor-link.php', GATHERPRESS_CORE_PATH ),
 				array(
-					'gatherpress_contributor' => $gatherpress_contributor,
+					'gatherpress_contributor'     => $gatherpress_contributor,
+					'gatherpress_contributor_end' => $gatherpress_contributor_end,
 				),
 				true
 			);
 		endforeach;
 		?>
-	</ul>
+	</div>
 </div>
