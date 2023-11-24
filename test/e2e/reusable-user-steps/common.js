28aaa79aa0f5153b34f94ec25d3b5f5ed4da4a10
Mike Auteri <mauteri@gmail.com>
2023-11-24 11:15:18 -0500

    Fix JSHint.

--- test/e2e/reusable-user-steps/common.js
+++ test/e2e/reusable-user-steps/common.js
@@ -1,24 +1,27 @@
 /**
  * Login user flow
- * @param {*} page
- * @param {*} username
- * @param {*} password
+ * @param {*} root0
+ * @param {*} root0.page
+ * @param {*} root0.username
+ * @param {*} root0.password
  */
-const login = async ({ page, username, password = process.env.WP_ADMIN_PASSWORD }) => {
-	page.goto("/wp-login.php", {
+const login = async ({
+	page,
+	username,
+	password = process.env.WP_ADMIN_PASSWORD,
+}) => {
+	page.goto('/wp-login.php', {
 		timeout: 40000,
 	});
 
-	await page.getByLabel("Username or Email Address").isVisible();
-	await page.getByLabel("Username or Email Address").fill(username);
+	await page.getByLabel('Username or Email Address').isVisible();
+	await page.getByLabel('Username or Email Address').fill(username);
 
-	await page.getByLabel("Password", { exact: true }).isVisible();
+	await page.getByLabel('Password', { exact: true }).isVisible();
 
-	await page
-		.getByLabel("Password", { exact: true })
-		.fill(password);
+	await page.getByLabel('Password', { exact: true }).fill(password);
 
-	await page.getByRole("button", { name: "Log In" }).click();
+	await page.getByRole('button', { name: 'Log In' }).click();
 };
 
 module.exports = { login };
