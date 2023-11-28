591f4a3b043761fd5f7d4f7eef22ae1b5ab9c2c1
Mike Auteri <mauteri@gmail.com>
2023-11-28 08:41:13 -0500

    Run build and update lock.

--- build/blocks/online-event/online-event.asset.php
+++ build/blocks/online-event/online-event.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '4ad7014b0fafccb1931a');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'd11dd41d5aa9462e6932');
