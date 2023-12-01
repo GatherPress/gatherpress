28c7b3d14a60ac9964f53c1115c38c76fed296e3
prayag.m_pristine <prayagmankar1@gmail.com>
2023-12-01 11:59:00 +0530

    updated test name and description

--- test/e2e/admin-tests/gather-press-admin-venue.spec.js
+++ test/e2e/admin-tests/gather-press-admin-venue.spec.js
@@ -1,7 +1,7 @@
 const { test } = require("@playwright/test");
 const { login } = require("../reusable-user-steps/common.js");
 
-test.describe("As admin login into gatherPress", () => {
+test.describe(" Validate test for admin side venue test", () => {
 	test.beforeEach(async ({ page }) => {
 		test.setTimeout(120000);
 		await page.setViewportSize({ width: 1920, height: 720 });
@@ -9,7 +9,7 @@ test.describe("As admin login into gatherPress", () => {
 	});
 
 	
-	test("Verify the admin can create Venue",async({page})=>{
+	test("The admin should create a new post for Venue",async({page})=>{
 
 		await login({ page, username: 'testuser1' });
 
