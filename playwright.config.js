11aa0979efaac8673e308aabf10c396709e1cae7
prashant bellad <prashantabellad@gmail.com>
2023-12-06 09:16:17 +0530

    config: set retries to 2 for playwright tests

--- playwright.config.js
+++ playwright.config.js
@@ -17,7 +17,7 @@ module.exports = defineConfig({
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: !!process.env.CI,
 	/* Retry on CI only */
-	retries: process.env.CI ? 2 : 2,
+	retries: 2,
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
 
