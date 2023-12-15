67ed08b524f68d68993462e4d88afcff492a496e
prayag.m_pristine <prayagmankar1@gmail.com>
2023-12-15 22:39:29 +0530

    venue test file change the double quote to single quote

--- test/e2e/admin-tests/gatherpress-admin-venue.spec.js
+++ test/e2e/admin-tests/gatherpress-admin-venue.spec.js
@@ -1,7 +1,7 @@
 const { test } = require("@playwright/test");
 const { login } = require("../reusable-user-steps/common.js");
 
-test.describe(" e2e test for venue through admin side", () => {
+test.describe("e2e test for venue through admin side", () => {
 	test.beforeEach(async ({ page }) => {
 		test.setTimeout(120000);
 		await page.setViewportSize({ width: 1920, height: 720 });
