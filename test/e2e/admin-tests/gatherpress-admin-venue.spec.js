a83655f048e56bceaf21a34f422337708cbb4b0f
prayag.m_pristine <prayagmankar1@gmail.com>
2023-12-16 09:05:03 +0530

    update the venue code remove double qotes

--- test/e2e/admin-tests/gatherpress-admin-venue.spec.js
+++ test/e2e/admin-tests/gatherpress-admin-venue.spec.js
@@ -1,41 +1,41 @@
-const { test } = require("@playwright/test");
-const { login } = require("../reusable-user-steps/common.js");
+const { test } = require('@playwright/test');
+const { login } = require('../reusable-user-steps/common.js');
 
-test.describe("e2e test for venue through admin side", () => {
+test.describe('e2e test for venue through admin side', () => {
 	test.beforeEach(async ({ page }) => {
 		test.setTimeout(120000);
 		await page.setViewportSize({ width: 1920, height: 720 });
-		await page.waitForLoadState("networkidle");
+		await page.waitForLoadState('networkidle');
 	});
 
-	test("The admin should be able to create a new post for Venue", async ({
+	test('The admin should be able to create a new post for Venue', async ({
 		page,
 	}) => {
-		await login({ page, username: "testuser1" });
+		await login({ page, username: 'testuser1' });
 
-		await page.getByRole("link", { name: "Events", exact: true }).click();
+		await page.getByRole('link', { name: 'Events', exact: true }).click();
 
-		await page.getByRole("link", { name: "Venues" }).click();
-		await page.screenshot({ path: "vanue-page.png" });
+		await page.getByRole('link', { name: 'Venues' }).click();
+		await page.screenshot({ path: 'vanue-page.png' });
 
 		await page
-			.locator("#wpbody-content")
-			.getByRole("link", { name: "Add New" })
+			.locator('#wpbody-content')
+			.getByRole('link', { name: 'Add New' })
 			.click();
 
-		await page.getByLabel("Add title").isVisible();
-		await page.getByLabel("Add title").fill("Test venue");
-		await page.getByLabel("Add title").press("Tab");
+		await page.getByLabel('Add title').isVisible();
+		await page.getByLabel('Add title').fill('Test venue');
+		await page.getByLabel('Add title').press('Tab');
 
-		const venue = await page.$(".gp-venue__name");
-		await venue.press("Backspace");
+		const venue = await page.$('.gp-venue__name');
+		await venue.press('Backspace');
 
 		await page
-			.getByLabel("Empty block; start writing or")
-			.fill("test venue information");
+			.getByLabel('Empty block; start writing or')
+			.fill('test venue information');
 
-		await page.getByLabel("Toggle block inserter").click();
-		await page.getByRole("option", { name: "Paragraph" }).click();
-		await page.screenshot({ path: "new-venue.png" });
+		await page.getByLabel('Toggle block inserter').click();
+		await page.getByRole('option', { name: 'Paragraph' }).click();
+		await page.screenshot({ path: 'new-venue.png' });
 	});
 });
