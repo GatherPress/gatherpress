86e6028f723021b4ab0c681bfb6e62051078c9ee
Patricia BT <hello@patriciabt.com>
2023-12-18 05:59:35 +0100

    i18n load textdomain and language files
    Find language files in gatherpress/languages when missing in wp-content/languages/plugins/
    
    The translation files will be in wp-content/languages/plugins/ once the plugin on the repository and translated in translate.wordpress.org.
    Until that, we need to load from /languages folder and load the textdomain.
    See https://developer.wordpress.org/plugins/internationalization/how-to-internationalize-your-plugin/#plugins-on-wordpress-org

--- gatherpress.php
+++ gatherpress.php
@@ -4,10 +4,11 @@
  * Plugin URI:          https://gatherpress.org/
  * Description:         Powering Communities with WordPress.
  * Author:              The GatherPress Community
- * Author URI:          https://gatherpess.org/
+ * Author URI:          https://gatherpress.org/
  * Version:             0.26.0
  * Minimum PHP Version: 7.4
  * Text Domain:         gatherpress
+ * Domain Path:         /languages
  * License:             GPLv2 or later (license.txt)
  *
  * This file serves as the main plugin file for GatherPress. It defines the plugin's basic information,
@@ -28,6 +29,17 @@ define( 'GATHERPRESS_REST_NAMESPACE', 'gatherpress/v1' );
 if ( ! require_once GATHERPRESS_CORE_PATH . '/includes/core/requirements-check.php' ) {
 	return;
 }
+
+// Load the plugin textdomain 'gatherpress' and find language files in gatherpress/languages when missing in wp-content/languages/plugins/
+function gatherpress_load_textdomain( $mofile, $domain ) {
+	if ( 'gatherpress' === $domain && false !== strpos( $mofile, WP_LANG_DIR . '/plugins/' ) ) {
+		$locale = apply_filters( 'plugin_locale', determine_locale(), $domain );
+		$mofile = WP_PLUGIN_DIR . '/' . dirname( plugin_basename( __FILE__ ) ) . '/languages/' . $domain . '-' . $locale . '.mo';
+	}
+	return $mofile;
+}
+
+load_plugin_textdomain( 'gatherpress', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
 // Include and register the autoloader class for automatic loading of plugin classes.
 require_once GATHERPRESS_CORE_PATH . '/includes/core/classes/class-autoloader.php';
