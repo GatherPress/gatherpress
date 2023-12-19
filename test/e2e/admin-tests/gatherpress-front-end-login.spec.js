f8b4469724599f357a37cddff9cf43a849b69fc6
prayag.m_pristine <prayagmankar1@gmail.com>
2023-12-19 15:34:53 +0530

    login test for front end user

---
+++ test/e2e/admin-tests/gatherpress-front-end-login.spec.js
@@ -0,0 +1,35 @@
+const { test } = require('@playwright/test');
+const { login } = require('../reusable-user-steps/common.js');
+
+test.describe('e2e test for login to front end guests', () => {
+    const dev_url = 'https://develop.gatherpress.org/';
+	test.beforeEach(async ({ page }) => {
+		test.setTimeout(60000);
+		await page.setViewportSize({ width: 1920, height: 720 });
+		await page.waitForLoadState('networkidle');
+	});
+
+	test('verify that the user is able to login after click on RSVP >> login', async ({
+		page,
+	}) => {
+		await page.goto(dev_url);
+        await page.screenshot({path:'homepage.png', fullPage:true});
+
+		await page.getByRole('link', { name: 'RSVP' }).click();
+
+		await page.getByText('Login', { exact: true }).click();
+        
+        await login({ page, username: 'testuser1' });
+
+        await page.goto(dev_url);
+
+		await page.getByRole('link', { name: 'RSVP' }).click();
+
+		await page.locator('a').filter({ hasText: 'Attend' }).click();
+
+        await page.waitForLoadState('networkidle');
+
+        await page.screenshot({path:'post-attendies.png', fullPage:true});
+        
+	});
+});
