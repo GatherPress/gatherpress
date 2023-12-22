5f90c884bbb70be3b1560b5d999adc4015cabb7f
Mike Auteri <mauteri@gmail.com>
2023-12-22 10:19:28 -0500

    Fix linting issues.

--- test/unit/js/src/components/DateTime.test.js
+++ test/unit/js/src/components/DateTime.test.js
@@ -1,4 +1,15 @@
-import { DateTimeStartLabel, DateTimeEndLabel } from '../../../../../src/components/DateTime';
+/**
+ * External dependencies.
+ */
+import { expect, test } from '@jest/globals';
+
+/**
+ * Internal dependencies.
+ */
+import {
+	DateTimeStartLabel,
+	DateTimeEndLabel,
+} from '../../../../../src/components/DateTime';
 
 /**
  * Coverage for DateTimeStartLabel.
@@ -6,7 +17,7 @@ import { DateTimeStartLabel, DateTimeEndLabel } from '../../../../../src/compone
 test('DateTimeStartLabel displays correct format', () => {
 	const props = {
 		dateTimeStart: '2023-12-28T10:26:00',
-	}
+	};
 
 	expect(DateTimeStartLabel(props)).toBe('December 28, 2023 10:26 am');
 });
@@ -17,7 +28,7 @@ test('DateTimeStartLabel displays correct format', () => {
 test('DateTimeEndLabel displays correct format', () => {
 	const props = {
 		dateTimeEnd: '2023-12-28T10:26:00',
-	}
+	};
 
 	expect(DateTimeEndLabel(props)).toBe('December 28, 2023 10:26 am');
 });
