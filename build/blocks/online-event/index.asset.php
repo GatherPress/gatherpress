225905c0ecc7d40a3258cc415e967977df5def61
Mike Auteri <mauteri@gmail.com>
2024-02-16 21:44:28 -0500

    Updates for version 0.28.0

--- build/blocks/online-event/index.asset.php
+++ build/blocks/online-event/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9a2c88cc2643e563f963');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd34d4db071c66f0e88b9');
