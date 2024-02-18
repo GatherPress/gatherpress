8d324f77e67dade6fbb47924c2de375d0732c007
Mike Auteri <mauteri@gmail.com>
2024-02-17 21:04:20 -0500

    Check post type of event for hasEventPast function.

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-i18n'), 'version' => '3a1b37d47119cd0dd1c3');
+<?php return array('dependencies' => array('moment', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-i18n'), 'version' => 'c6fce8fab64b6d9302cf');
