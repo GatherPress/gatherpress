8e63fddd34a9ab3f3e2483c447af036c8adfbe32
Caleb Matteis <matteismusic@gmail.com>
2024-03-01 02:19:18 -0500

    Fixed React code smells

--- src/components/MaxAttendance.js
+++ src/components/MaxAttendance.js
@@ -2,9 +2,10 @@
  * WordPress dependencies.
  */
 import { __ } from '@wordpress/i18n';
+// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
 import { __experimentalNumberControl as NumberControl } from '@wordpress/components';
 import { useState, useEffect, useCallback } from '@wordpress/element';
-import { select, useDispatch, useSelect } from '@wordpress/data';
+import { useDispatch, useSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies.
@@ -22,48 +23,51 @@ import { getFromGlobal } from '../helpers/globals';
  * @return {JSX.Element} A number control for setting the maximum attendance limit.
  */
 const MaxAttendance = () => {
-    const { editPost, unlockPostSaving } = useDispatch( 'core/editor' ); // Make sure to correct typo from `unLockPostSaving` to `unlockPostSaving`
-    const isNewEvent = useSelect( (select) => { 
-        return select( 'core/editor' ).isCleanNewPost();
-    }, [] );
+	const { editPost, unlockPostSaving } = useDispatch('core/editor'); // Make sure to correct typo from `unLockPostSaving` to `unlockPostSaving`
+	const isNewEvent = useSelect((select) => {
+		return select('core/editor').isCleanNewPost();
+	}, []);
 
-    let defaultMaxAttendance = useSelect( ( select ) => { // Correct typo from `defaultMaxAttendane` to `defaultMaxAttendance`
-        return select( 'core/editor' ).getEditedPostAttribute( 'meta' ).max_attendance;
-    }, []);
+	// eslint-disable-next-line no-shadow
+	let defaultMaxAttendance = useSelect((select) => {
+		// Correct typo from `defaultMaxAttendane` to `defaultMaxAttendance`
+		return select('core/editor').getEditedPostAttribute('meta')
+			.max_attendance;
+	}, []);
 
-    if (isNewEvent) {
-        defaultMaxAttendance = getFromGlobal( 'settings.maxAttendance' );
-    }
+	if (isNewEvent) {
+		defaultMaxAttendance = getFromGlobal('settings.maxAttendance');
+	}
 
-    const [maxAttendance, setMaxAttendance] = useState( defaultMaxAttendance );
+	const [maxAttendance, setMaxAttendance] = useState(defaultMaxAttendance);
 
-    const updateMaxAttendance = useCallback(
-        (value) => {
-            const meta = { max_attendance: Number( value ) };
+	const updateMaxAttendance = useCallback(
+		(value) => {
+			const meta = { max_attendance: Number(value) };
 
-            setMaxAttendance( value );
-            editPost( { meta } );
-            unlockPostSaving(); // Call `unlockPostSaving` here to unlock the save button after updating the meta
-        },
-        [editPost, unlockPostSaving]
-    );
+			setMaxAttendance(value);
+			editPost({ meta });
+			unlockPostSaving(); // Call `unlockPostSaving` here to unlock the save button after updating the meta
+		},
+		[editPost, unlockPostSaving]
+	);
 
-    useEffect( () => {
-        if ( isNewEvent && defaultMaxAttendance !==0 ) {
-            updateMaxAttendance( defaultMaxAttendance );
-        }
-    }, [isNewEvent, defaultMaxAttendance, updateMaxAttendance]);
+	useEffect(() => {
+		if (isNewEvent && defaultMaxAttendance !== 0) {
+			updateMaxAttendance(defaultMaxAttendance);
+		}
+	}, [isNewEvent, defaultMaxAttendance, updateMaxAttendance]);
 
-    return (
-        <NumberControl 
-            label={__( 'Maximum Attending Limit', 'gatherpress')}
-            value={ maxAttendance }
-            min={0}
-            onChange={ (value) => {
-                updateMaxAttendance( value );
-            } }
-        />
-    );
+	return (
+		<NumberControl
+			label={__('Maximum Attending Limit', 'gatherpress')}
+			value={maxAttendance}
+			min={0}
+			onChange={(value) => {
+				updateMaxAttendance(value);
+			}}
+		/>
+	);
 };
 
 export default MaxAttendance;
