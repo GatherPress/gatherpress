7af535781e7399ae4f97693968ccf0b7a9be2d9c
Mike Auteri <mauteri@gmail.com>
2024-05-21 10:44:33 -0400

    Revert updates to package.json, issues with tests.

--- build/blocks/online-event/index.asset.php
+++ build/blocks/online-event/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a70fa9eb131c2d08f700');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '22c4d9952ae84d4f8973');
--- build/blocks/rsvp/rsvp.asset.php
+++ build/blocks/rsvp/rsvp.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '7cd3eab6c4119d2e6253');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '9c0620b4f014481d9eb4');
