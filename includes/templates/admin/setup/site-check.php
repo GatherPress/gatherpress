568c8186037374f1649fd5338369a9a78d95a3e8
Paul Barthmaier <pbrocks@users.noreply.github.com>
2024-06-04 11:23:13 -0400

    change the option name

--- includes/templates/admin/setup/site-check.php
+++ includes/templates/admin/setup/site-check.php
@@ -14,19 +14,20 @@ defined( 'ABSPATH' ) || exit; // @codeCoverageIgnore
 		<div>
 			<?php
 			printf(
-				/* translators: %s: "enabling user registration here" (hyperlinked) */
-				esc_html__( 'To ensure GatherPress functions optimally, we recommend enabling user registration and setting your site\'s timezone. You can do so by %s and %s', 'gatherpress' ),
+				/* translators: %1$s: "enabling user registration here" (hyperlinked) */
+				/* translators: %2$s: "setting time zone here" (hyperlinked) */
+				esc_html__( 'To ensure GatherPress functions optimally, we recommend enabling user registration and setting your site\'s timezone. You can do so by %1$s and %2$s', 'gatherpress' ),
 				'<a href=' . esc_url( admin_url( 'options-general.php#users_can_register' ) ) . '>'
-				. esc_html_x( 'enabling user registration here', 'Context: To ensure GatherPress functions optimally, user registration and setting your site\'s timezone. You can do so by %s and %s.', 'gatherpress' )
+				. esc_html_x( 'enabling user registration here', 'Context: To ensure GatherPress functions optimally, user registration and setting your site\'s timezone. You can do so by %1$s and %2$s.', 'gatherpress' )
 				. '</a>',
 				'<a href=' . esc_url( admin_url( 'options-general.php#timezone_string' ) ) . '>'
-				. esc_html_x( 'setting time zone here', 'Context: To ensure GatherPress functions optimally, user registration and setting your site\'s timezone. You can do so by %s and %s.', 'gatherpress' )
+				. esc_html_x( 'setting time zone here', 'Context: To ensure GatherPress functions optimally, user registration and setting your site\'s timezone. You can do so by %1$s and %2$s.', 'gatherpress' )
 				. '</a>'
 			);
 			?>
 		</div>
 		<div>
-			<a href="<?php echo esc_url( wp_nonce_url( add_query_arg( 'action', 'gatherpress_suppress_membership_notification' ), 'clear-notification' ) ); ?>" class="button button-secondary">
+			<a href="<?php echo esc_url( wp_nonce_url( add_query_arg( 'action', 'gatherpress_suppress_site_notification' ), 'clear-notification' ) ); ?>" class="button button-secondary">
 				<?php esc_html_e( 'Dismiss forever', 'gatherpress' ); ?>
 			</a>
 		</div>
