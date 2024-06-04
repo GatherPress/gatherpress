6b764556077d94396ca176542b302d8bb29e818c
Paul Barthmaier <pbrocks@users.noreply.github.com>
2024-06-04 11:42:10 -0400

    translators

--- includes/templates/admin/setup/site-check.php
+++ includes/templates/admin/setup/site-check.php
@@ -14,8 +14,7 @@ defined( 'ABSPATH' ) || exit; // @codeCoverageIgnore
 		<div>
 			<?php
 			printf(
-				/* translators: %1$s: "enabling user registration here" (hyperlinked) */
-				/* translators: %2$s: "setting time zone here" (hyperlinked) */
+				/* translators: 1: Enabling user registration 2: Setting time zone */
 				esc_html__( 'To ensure GatherPress functions optimally, we recommend enabling user registration and setting your site\'s timezone. You can do so by %1$s and %2$s', 'gatherpress' ),
 				'<a href=' . esc_url( admin_url( 'options-general.php#users_can_register' ) ) . '>'
 				. esc_html_x( 'enabling user registration here', 'Context: To ensure GatherPress functions optimally, user registration and setting your site\'s timezone. You can do so by %1$s and %2$s.', 'gatherpress' )
