e6aad11d5d1c7347d4e7e91c0b0b3f13c93aa937
Mike Auteri <mauteri@gmail.com>
2024-06-05 09:31:19 -0400

    Fix PHPCS issue.

--- includes/data/credits/credits.php
+++ includes/data/credits/credits.php
@@ -20,17 +20,17 @@ return array(
 		'gatherpress-team' => array( 'jmarx', 'pbrocks', 'prayagm', 'linusx007', 'stephenerdelyi', 'carstenbach', 'calebthedev' ),
 		'contributors'     => array( 'hauvong', 'phoopee3', 'cameronbarrett', 'courane01', 'newyorkerlaura', 'jeffpaul', 'deshabhishek007', 'michelleames', 'prashantabellad', 'javiercasares', 'meaganhanes', 'nilovelez' ),
 	),
-	'0.29.0' => array(
+	'0.29.0'       => array(
 		'project-leaders'  => array( 'mauteri', 'hrmervin', 'patricia70' ),
 		'gatherpress-team' => array( 'jmarx', 'pbrocks', 'prayagm', 'linusx007', 'stephenerdelyi', 'carstenbach', 'calebthedev' ),
 		'contributors'     => array( 'hauvong', 'phoopee3', 'cameronbarrett', 'courane01', 'newyorkerlaura', 'jeffpaul', 'deshabhishek007', 'michelleames', 'prashantabellad', 'javiercasares', 'meaganhanes', 'nilovelez' ),
 	),
-	'0.28.0' => array(
+	'0.28.0'       => array(
 		'project-leaders'  => array( 'mauteri', 'hrmervin', 'patricia70' ),
 		'gatherpress-team' => array( 'jmarx', 'meaganhanes', 'prashantabellad', 'prayagm', 'javiercasares', 'linusx007' ),
 		'contributors'     => array( 'hauvong', 'phoopee3', 'cameronbarrett', 'courane01', 'newyorkerlaura', 'calebthedev', 'pbrocks', 'jeffpaul', 'deshabhishek007', 'michelleames', 'carstenbach' ),
 	),
-	'0.27.0' => array(
+	'0.27.0'       => array(
 		'project-leaders'  => array( 'mauteri', 'hrmervin' ),
 		'gatherpress-team' => array( 'jmarx', 'meaganhanes', 'pbrocks', 'patricia70' ),
 		'contributors'     => array( 'hauvong', 'phoopee3', 'cameronbarrett', 'prashantabellad', 'courane01', 'newyorkerlaura', 'calebthedev', 'prayagm' ),
