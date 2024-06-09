5300d85676374c68d6a32fc41eb97d16aa283dd8
Carsten Bach <mail@carsten-bach.de>
2024-06-09 21:38:25 +0200

    Cleanup

--- src/panels/venue-settings/slotfill.js
+++ src/panels/venue-settings/slotfill.js
@@ -5,11 +5,7 @@
  */
 import { __ } from '@wordpress/i18n';
 
-// taken from https://developer.wordpress.org/block-editor/how-to-guides/metabox/
-import { useEntityProp } from '@wordpress/core-data';
-import { useSelect } from '@wordpress/data';
-
-import { TextControl, Fill } from '@wordpress/components';
+import { Fill } from '@wordpress/components';
 
 
 
@@ -21,35 +17,10 @@ import { VenuePluginDocumentSettings } from './slot.js';
 
 
 export default function VenuePluginFill() {
-	// const post = useSelect(
-	// 	(select) => select('core/editor').getCurrentPost(),
-	// 	[]
-	// );
-	// const [meta, setMeta] = useEntityProp(
-	// 	'postType',
-	// 	post.type,
-	// 	'meta',
-	// 	post.id
-	// );
-	// const metaFieldValue = meta[DURATION_META];
-	// const updateMetaValue = (newValue) => {
-	// 	setMeta({ ...meta, [DURATION_META]: newValue });
-	// };
 
 	return (
 		<>
 			<Fill name="EventPluginDocumentSettings">
-				{/* <TextControl
-					label={__('Duration', 'theater-production-blocks')}
-					type="number"
-					isShiftStepEnabled
-					shiftStep="5"
-					step="5"
-					min={0}
-					placeholder="80"
-					value={metaFieldValue}
-					onChange={updateMetaValue}
-				/> */}
 				<p>THE "VenuePluginDocumentSettings" in EventPluginDocumentSettings</p>
 			</Fill>
 			<Fill name="EventPluginDocumentSettings">
