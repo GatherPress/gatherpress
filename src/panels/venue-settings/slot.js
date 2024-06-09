897f7fc730696159fa36f4f815f1660a70c3777d
Carsten Bach <mail@carsten-bach.de>
2024-06-09 21:38:47 +0200

    DEBUG WIP

--- src/panels/venue-settings/slot.js
+++ src/panels/venue-settings/slot.js
@@ -9,10 +9,25 @@ import { createSlotFill, PanelRow } from '@wordpress/components';
 
 export const { Fill, Slot } = createSlotFill('VenuePluginDocumentSettings');
 
-export const VenuePluginDocumentSettings = ({ children, className }) => (
+// export const VenuePluginDocumentSettings = ({ children, className }) => (
+export const VenuePluginDocumentSettings = ( props ) => {
+	console.log(props);
+	return (
+
 	<Fill>
-		<PanelRow className={className}>{children}</PanelRow>
+		<PanelRow className={props.className}>{props.children}</PanelRow>
 	</Fill>
-);
+
+// <SlotFillProvider>
+// 	<Panel header="Panel with slot">
+// 		<PanelBody>
+// 			<Slot name="ExampleSlot"/>
+// 		</PanelBody>
+// 	</Panel>
+// 	<Fill name="ExampleSlot" >
+// 		Panel body
+// 	</Fill>
+// </SlotFillProvider>
+)};
 
 VenuePluginDocumentSettings.Slot = Slot;
