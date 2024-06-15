39712728c4007f47eb9dc64740cef0156a45a3bd
Carsten Bach <mail@carsten-bach.de>
2024-06-15 21:44:14 +0200

    Add filter examples to docblock

--- includes/core/classes/class-migrate.php
+++ includes/core/classes/class-migrate.php
@@ -48,6 +48,36 @@ class Migrate {
 		/**
 		 * Filters the list of data-names and their respective export- and import-callbacks.
 		 *
+		 * The filter allows to hook into WordPress' native import & export processes,
+		 * when post types of the GatherPress plugin are being migrated.
+		 * That can be helpful, if you want to import event- or venue-data from another plugin.
+		 *
+		 * @example
+		 *   The filter in use, to import data from the "Event Organiser" plugin.
+		 *   https://github.com/carstingaxion/gatherpress-export-import/blob/main/import-events-from--event-organiser.php
+		 *
+		 * @example
+		 *   Example use of the filter to illustrate function signatures for the callbacks.
+		 *   \add_filter(
+		 *       'gatherpress_pseudopostmetas',
+		 *       function ( array $pseudopostmetas ): array {
+		 *           $pseudopostmetas['my_gatherpress_extension_data_name'] = [
+		 *               'export_callback' => function ( WP_Post $post ): string {
+		 *                   // Do something with $post.
+		 *                   // Query & prepare custom data
+		 *                   // to exported with the current post.
+		 *                   return 'my_gatherpress_extension_data';
+		 *               },
+		 *               'import_callback' => function (int $post_id, $meta_value ): void {
+		 *                   // Save data for given post_id to a custom location,
+		 *                   // when data should not end up in the postmeta table.
+		 *                   return;
+		 *               },
+		 *           ];
+		 *           return $pseudopostmetas;
+		 *       }
+		 *   );
+		 *
 		 * @since 1.0.0
 		 *
 		 * @param  array $pseudopostmetas List of data-names and their respective export- and import-callbacks.
