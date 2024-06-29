5b90af1a606ce3083b99fb1ee0b57de64e02d800
Jeffrey Marx <jeffrey_marx@yahoo.com>
2024-06-29 13:13:04 -0400

    Build files

--- build/blocks/online-event/index.asset.php
+++ build/blocks/online-event/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '22c4d9952ae84d4f8973');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0841ef0832eba8b43eba');
--- build/blocks/venue/index.asset.php
+++ build/blocks/venue/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => 'e251b1f547ecd23dd36a');
+<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '8713f7b95efeb8dc88df');
