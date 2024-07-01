8a6e96057e66997c61dfe2e177db4be38f37686d
01Prayag <prayag.m@pristineprotech.com>
2024-07-01 10:27:58 +0530

    format the code using prettier

--- test/e2e/admin-tests/gatherpress-event-nonLoggegIn.spec.js
+++ test/e2e/admin-tests/gatherpress-event-nonLoggegIn.spec.js
@@ -1,45 +1,45 @@
-const { test, expect } = require("@playwright/test");
-const { login_user } = require("../reusable-user-steps/user-login");
-const { login } = require("../reusable-user-steps/common");
+const { test, expect } = require('@playwright/test');
+const { login_user } = require('../reusable-user-steps/user-login');
+const { login } = require('../reusable-user-steps/common');
 
-test.describe("e2e test for home page event on develop.gatherpress.org", () => {
+test.describe('e2e test for home page event on develop.gatherpress.org', () => {
 	test.beforeEach(async ({ page }) => {
 		test.setTimeout(120000);
 		await page.setViewportSize({ width: 1920, height: 720 });
-		await page.waitForLoadState("networkidle");
+		await page.waitForLoadState('networkidle');
 	});
 });
 
-test("01-e2e test for publish the offline event", async ({ page }) => {
-	await login({ page, username: "testuser1" });
+test('01-e2e test for publish the offline event', async ({ page }) => {
+	await login({ page, username: 'testuser1' });
 
-	await page.getByRole("link", { name: "Events", exact: true }).click();
+	await page.getByRole('link', { name: 'Events', exact: true }).click();
 	await page
-		.locator("#wpbody-content")
-		.getByRole("link", { name: "Add New" })
+		.locator('#wpbody-content')
+		.getByRole('link', { name: 'Add New' })
 		.click();
 
-	const currentDate = new Date().toISOString().split("T")[0]; // format YYYY-MM-DD
+	const currentDate = new Date().toISOString().split('T')[0]; // format YYYY-MM-DD
 	const eventTitle = await page
-		.getByLabel("Add title")
+		.getByLabel('Add title')
 		.fill(`offline T-Event:${currentDate}`);
 
 	await page
-		.getByLabel("Block: Event Date")
-		.locator("div")
+		.getByLabel('Block: Event Date')
+		.locator('div')
 		.first()
 		.isVisible();
-	await page.getByRole("heading", { name: "Date & time" }).isVisible();
+	await page.getByRole('heading', { name: 'Date & time' }).isVisible();
 
-	await page.getByRole("button", { name: "Event settings" }).click();
+	await page.getByRole('button', { name: 'Event settings' }).click();
 	await page
-		.locator("#inspector-select-control-1")
-		.selectOption("offline event");
+		.locator('#inspector-select-control-1')
+		.selectOption('offline event');
 
-	await page.getByRole("button", { name: "Publish", exact: true }).click();
+	await page.getByRole('button', { name: 'Publish', exact: true }).click();
 	await page
-		.getByLabel("Editor publish")
-		.getByRole("button", { name: "Publish", exact: true })
+		.getByLabel('Editor publish')
+		.getByRole('button', { name: 'Publish', exact: true })
 		.click();
 
 	await page
@@ -47,43 +47,43 @@ test("01-e2e test for publish the offline event", async ({ page }) => {
 		.isVisible({ timeout: 60000 }); //verified the event is live.
 	await expect(
 		page
-			.locator(".post-publish-panel__postpublish-buttons")
-			.filter({ hasText: "View Event" }),
+			.locator('.post-publish-panel__postpublish-buttons')
+			.filter({ hasText: 'View Event' }),
 	).toBeVisible(); //verify the view event button.
 });
 
-test("02-verify the non-logged in user view RSVP button on home page and do RSVP", async ({
+test('02-verify the non-logged in user view RSVP button on home page and do RSVP', async ({
 	page,
 }) => {
-	await page.goto("https://develop.gatherpress.org");
+	await page.goto('https://develop.gatherpress.org');
 
-	await page.getByRole("heading", { name: "Upcoming Events" }).click();
+	await page.getByRole('heading', { name: 'Upcoming Events' }).isVisible();
 	await page
-		.locator("div")
+		.locator('div')
 		.filter({ hasText: /^online Test Event$/ })
 		.isVisible();
 
-	await page.getByRole("link", { name: "RSVP" }).first().isVisible();
+	await page.getByRole('link', { name: 'RSVP' }).first().isVisible();
 
-	await page.getByRole("link", { name: "RSVP" }).first().click();
-	await page.getByText("Login", { exact: true }).click();
+	await page.getByRole('link', { name: 'RSVP' }).first().click();
+	await page.getByText('Login', { exact: true }).click();
 
-	await login_user({ page, username: "testuser1" });
+	await login_user({ page, username: 'testuser1' });
 	await page.evaluate(() => window.scrollTo(0, 1000));
 
 	await page
-		.getByRole("link", { name: "RSVP" })
+		.getByRole('link', { name: 'RSVP' })
 		.first()
 		.click({ timeout: 60000 });
 
-	await page.locator("a").filter({ hasText: "Attend" }).click();
-	await page.getByText("Close").click();
-	await page.locator(".gatherpress-rsvp-response__items").first().isVisible(); // verified the RSVP button is visible
-	await expect(page.getByText("Attending").first()).toBeVisible(); // verified the attending text after RSVP action.
+	await page.locator('a').filter({ hasText: 'Attend' }).click();
+	await page.getByText('Close').click();
+	await page.locator('.gatherpress-rsvp-response__items').first().isVisible(); // verified the RSVP button is visible
+	await expect(page.getByText('Attending').first()).toBeVisible(); // verified the attending text after RSVP action.
 
-	await page.locator(".gatherpress-rsvp-response__items").first().isVisible(); // verified the attending users list.
+	await page.locator('.gatherpress-rsvp-response__items').first().isVisible(); // verified the attending users list.
 	await page
-		.locator(".gatherpress-rsvp-response__items")
+		.locator('.gatherpress-rsvp-response__items')
 		.first()
-		.screenshot({ path: "attending.png" });
+		.screenshot({ path: 'attending.png' });
 });
