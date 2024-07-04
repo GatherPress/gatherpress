3048db5a632a5a2703f5c7b0fad863739f7d8c48
01Prayag <prayag.m@pristineprotech.com>
2024-07-04 11:26:22 +0530

    coding standard clear

--- test/e2e/admin-tests/gatherpress-event-nonLoggegIn.spec.js
+++ test/e2e/admin-tests/gatherpress-event-nonLoggegIn.spec.js
@@ -43,9 +43,7 @@ test('01-the user should publish the offline event', async ({ page }) => {
 	await page
 		.getByText(`${eventTitle} is now live.`)
 		.isVisible({ timeout: 60000 }); //verified the event is live.
-	await expect(
-		page
-			.locator('.post-publish-panel__postpublish-buttons')
+	await expect(page.locator('.post-publish-panel__postpublish-buttons')
 			.filter({ hasText: 'View Event' })).toBeVisible(); //verify the view event button.
 });
 
