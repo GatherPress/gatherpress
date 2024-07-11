90dd9364ade6e9c7b52ad381915d2c70b9c3d3b2
Jeffrey Marx <jeffrey_marx@yahoo.com>
2024-07-11 06:56:54 -0400

    Attach 'L' variable to the window object to avoid undefined errors

--- build/blocks/venue/venue.asset.php
+++ build/blocks/venue/venue.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'e5e7f5ed5fd48bb7c8cf');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'aebf8125f13faeb834d0');
