316a18ddda964a8f42a70b5c759717c47ebd9c8a
Mike Auteri <mauteri@gmail.com>
2024-07-28 15:21:52 -0400

    PHPCS ignore.

--- includes/core/classes/class-event-setup.php
+++ includes/core/classes/class-event-setup.php
@@ -250,7 +250,7 @@ class Event_Setup {
 
 		$table = sprintf( Event::TABLE_FORMAT, $wpdb->prefix, Event::POST_TYPE );
 
-		$table_exists = $wpdb->get_var(
+		$table_exists = $wpdb->get_var( // phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
 			$wpdb->prepare(
 				'SHOW TABLES LIKE %s',
 				$table
