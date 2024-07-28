32e4bad5d83eb803d98ad3a56dc5397a23a12d90
Mike Auteri <mauteri@gmail.com>
2024-07-28 09:21:59 -0400

    Bump blocks.

--- build/blocks/add-to-calendar/index.asset.php
+++ build/blocks/add-to-calendar/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a2a79e176bbde08a7267');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c27eaf0a25faf160b41b');
--- build/blocks/venue/index.asset.php
+++ build/blocks/venue/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => 'a0f2b66a4e1c3d359c08');
+<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '88717602f004caca0bbb');
