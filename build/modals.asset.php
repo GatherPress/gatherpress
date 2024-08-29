aa6210fb8a61da084ba4b3e07642d9fa3eaffcf0
Carsten Bach <mail@carsten-bach.de>
2024-08-29 03:12:58 +0200

    BUILD

--- build/modals.asset.php
+++ build/modals.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '8b1278753e22acf843e1');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '741c4d67a173d381d8b3');
