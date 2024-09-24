b8fd761547c2e1ddb3a28c2c490dc52df64b78d1
Carsten Bach <mail@carsten-bach.de>
2024-09-24 22:46:32 +0200

    Fix for CS

--- test/e2e/tests/frontend/rsvp.spec.ts
+++ test/e2e/tests/frontend/rsvp.spec.ts
@@ -42,7 +42,7 @@ test.describe('RSVP to an event', () => {
 
 	test('A logged in user can perform RSVP action', async ({ page }) => {
 		await page.goto('/');
-		await page.getByRole('link', { name: 'Events' }).click({ force: true }); // Imported from of https://github.com/GatherPress/demo-data
+		await page.getByRole('link', { name: 'Events' }).click({ force: true }); // Imported from of https://github.com/GatherPress/gatherpress-demo-data
 
 		await page.evaluate(() => window.scrollTo(0, 5000));
 		await page
@@ -50,32 +50,21 @@ test.describe('RSVP to an event', () => {
 			.first()
 			.click({ force: true, timeout: 60000 });
 
-		await page.locator('a').filter({ hasText: 'Attend' }).click({ force: true });
+		await page
+			.locator('a')
+			.filter({ hasText: 'Attend' })
+			.click({ force: true });
 		await page.getByText('Close').click({ force: true });
 		await expect(
-			page
-				.locator('.gatherpress-rsvp-response__items')
-				.first()
-			)
-			.toBeVisible(); // verified the RSVP button is visible.
+			page.locator('.gatherpress-rsvp-response__items').first()
+		).toBeVisible(); // verified the RSVP button is visible.
+
+		await expect(page.getByText('Attending').first()).toBeVisible({
+			timeout: 30000,
+		}); // verified the logged in user perform RSVP action
 
 		await expect(
-			page
-				.getByText('Attending')
-				.first()
-			)
-			.toBeVisible({ timeout: 30000 }); // verified the logged in user perform RSVP action
-
-		await expect(
-			page
-				.locator('.gatherpress-rsvp-response__items')
-				.first()
-			)
-			.toBeVisible(); // verified the attending users list.
-
-		// await page
-		// 	.locator('.gatherpress-rsvp-response__items')
-		// 	.first()
-		// 	.screenshot({ path: 'attending.png' });
+			page.locator('.gatherpress-rsvp-response__items').first()
+		).toBeVisible(); // verified the attending users list.
 	});
 });
