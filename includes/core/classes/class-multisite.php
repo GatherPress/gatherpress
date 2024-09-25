9efca5e1d715ffec7276dab65e8e86e58ba54016
Carsten Bach <mail@carsten-bach.de>
2024-09-26 00:30:30 +0200

    Share Options across Multisite

---
+++ includes/core/classes/class-multisite.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Main class for managing GatherPress in a WordPress Multisite.
+ *
+ * This class handles .... in the GatherPress plugin.
+ *
+ * @package GatherPress/Core
+ * @since 1.0.0
+ */
+
+namespace GatherPress\Core;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit; // @codeCoverageIgnore
+
+use GatherPress\Core\Traits\Singleton;
+use GatherPress\Core\Utility;
+
+/**
+ * Class Multisite.
+ *
+ * Core class for managing GatherPress in a WordPress Multisite.
+ *
+ * @since 1.0.0
+ */
+class Multisite {
+	/**
+	 * Enforces a single instance of this class.
+	 */
+	use Singleton;
+
+	/**
+	 * Class constructor.
+	 *
+	 * This method initializes the object and sets up necessary hooks.
+	 *
+	 * @since 1.0.0
+	 */
+	protected function __construct() {
+		$this->setup_hooks();
+	}
+
+	/**
+	 * Set up hooks for various purposes.
+	 *
+	 * This method adds hooks for different purposes as needed.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	protected function setup_hooks(): void {
+		add_action( 'init', array( $this, 'init_shared_options' ), PHP_INT_MIN );
+	}
+
+	/**
+	 * Fires after WordPress has finished loading but before any headers are sent.
+	 */
+	public function init_shared_options(): void {
+
+		// DEBUG ONLY // Set Site option "gatherpress_shared_options" to enable feature, until UI is in place.
+		add_filter( sprintf( 'pre_site_option_%s', Utility::prefix_key( 'shared_options' ) ), '__return_true' );
+
+		if ( ! is_multisite() || ! get_site_option( Utility::prefix_key( 'shared_options' ) ) ) {
+			return;
+		}
+
+		add_filter( sprintf( 'pre_option_%s', Utility::prefix_key( 'general' ) ), array( $this, 'pre_option' ), 10, 3 );
+
+		if ( is_main_site() ) {
+			add_action( sprintf( 'update_option_%s', Utility::prefix_key( 'general' ) ), array( $this, 'update_option' ), 10, 3 );
+		}
+	}
+
+	/**
+	 * Filters the value of an existing option before it is retrieved.
+	 *
+	 * @param mixed  $pre_option    The value to return instead of the option value. This differs from <code>$default_value</code>, which is used as the fallback value in the event the option doesn't exist elsewhere in get_option(). Default false (to skip past the short-circuit).
+	 * @param string $option        Option name.
+	 * @return mixed The value to return instead of the option value. This differs from <code>$default_value</code>, which is used as the fallback value in the event the option doesn't exist elsewhere in get_option(). Default false (to skip past the short-circuit).
+	 */
+	public function pre_option( $pre_option, string $option ) {
+		return get_site_option( $option );
+	}
+
+	/**
+	 * Fires after the value of a specific option has been successfully updated.
+	 *
+	 * @param mixed  $old_value The old option value.
+	 * @param mixed  $value     The new option value.
+	 * @param string $option    Option name.
+	 */
+	public function update_option( $old_value, $value, string $option ): void {
+		update_site_option( $option, $value );
+	}
+}
