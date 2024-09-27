569c7c83b0e7f9281fee04b35bd5e4ac71450d51
Carsten Bach <mail@carsten-bach.de>
2024-09-27 03:05:40 +0200

    Replace isVisible() with expect( ... ).toBeVisible(), as per the suggestion by @WunderBart

--- test/e2e/tests/editor/events.spec.ts
+++ test/e2e/tests/editor/events.spec.ts
@@ -7,12 +7,12 @@ test.describe('Events in the Editor', () => {
 	test.beforeEach(async ({ admin, page }) => {
 		await admin.createNewPost({ postType: 'gatherpress_event' });
 		await page.getByLabel('Add title').fill('Change title to allow saving');
-		await page
-			.getByLabel('Block: Event Date')
+		const eventDateBlock = await page.getByLabel('Block: Event Date')
 			.locator('div')
-			.first()
-			.isVisible();
-		await page.getByRole('heading', { name: 'Date & time' }).isVisible();
+			.first();
+		await expect( eventDateBlock ).toBeVisible();
+		const eventDateHeading = await page.getByRole('heading', { name: 'Date & time' });
+		await expect( eventDateHeading ).toBeVisible();
 
 		// Open the Document -> Event settings panel.
 		const panelToggle = page.getByRole('button', {
