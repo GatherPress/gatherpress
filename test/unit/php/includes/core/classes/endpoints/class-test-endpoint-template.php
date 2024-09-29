313fafd7a8c15cd398850572312d6ec0c16907e2
Carsten Bach <mail@carsten-bach.de>
2024-09-29 23:18:33 +0200

    WIP unit tests

--- test/unit/php/includes/core/classes/endpoints/class-test-endpoint-template.php
+++ test/unit/php/includes/core/classes/endpoints/class-test-endpoint-template.php
@@ -2,11 +2,11 @@
 /**
  * Class handles unit tests for GatherPress\Core\Endpoints\Endpoint_Template.
  *
- * @package GatherPress\Core\Endpoints
+ * @package GatherPress\Core
  * @since 1.0.0
  */
 
-namespace GatherPress\Tests\Core;
+namespace GatherPress\Tests\Core\Endpoints;
 
 use GatherPress\Core\Endpoints\Endpoint_Template;
 use PMC\Unit_Test\Base;
@@ -19,29 +19,64 @@ use PMC\Unit_Test\Utility;
  * @group              endpoints
  */
 class Test_Endpoint_Template extends Base {
+	/**
+	 * Coverage for __construct method.
+	 *
+	 * @covers ::__construct
+	 *
+	 * @return void
+	 */
+	public function test___construct(): void {
+		$slug             = 'custom-endpoint';
+		$callback         = function () {
+			return array(
+				'file_name' => 'endpoint-template.php',
+				'dir_path'  => '/path/to/theme',
+			);
+		};
+		$plugin_default   = '/mock/plugin/templates';
+		$template_default = '/default/template.php';
+
+		// Create a mock for Endpoint_Template.
+		$instance = new Endpoint_Template( $slug, $callback, $plugin_default );
+
+		$this->assertInstanceOf( Endpoint_Template::class, $instance );
+	}
 
 	/**
 	 * Coverage for activate.
 	 *
-	 * @covers ::__construct
 	 * @covers ::activate
 	 *
 	 * @return void
-
+	*/
 	public function test_activate(): void {
-		$cb = function(){};
-		$instance = new Endpoint_Template( 'unit-test', $cb );
-		$hooks    = array(
-			array(
-				'type'     => 'filter',
-				'name'     => 'template_include',
-				'priority' => 10,
-				'callback' => array( $instance, 'template_include' ),
-			),
-		);
+		$slug             = 'custom-endpoint';
+		$callback         = function () {
+			return array(
+				'file_name' => 'endpoint-template.php',
+				'dir_path'  => '/path/to/theme',
+			);
+		};
+		$plugin_default   = '/mock/plugin/templates';
+		$template_default = '/default/template.php';
 
-		$this->assert_hooks( $hooks, $instance );
-	} */
+		// Create a mock for Endpoint_Template.
+		$instance = new Endpoint_Template( $slug, $callback, $plugin_default );
+		// var_dump($instance);
+		// $instance->activate();
+
+		// $hooks    = array(
+		// 	array(
+		// 		'type'     => 'filter',
+		// 		'name'     => 'template_include',
+		// 		'priority' => 10,
+		// 		'callback' => array( $instance, 'template_include' ),
+		// 	),
+		// );
+
+		// $this->assert_hooks( $hooks, $instance ); // DOES NOT WORK WITH NON-SINGLETONS, BUT WILL NOT THROW AN ERROR.
+	}
 
 	/**
 	 * Tests template_include method when the theme has the template.
@@ -62,12 +97,12 @@ class Test_Endpoint_Template extends Base {
 		$template_default = '/default/template.php';
 
 		// Create a mock for Endpoint_Template.
-		$endpoint = new Endpoint_Template( $slug, $callback, $plugin_default );
+		$instance = new Endpoint_Template( $slug, $callback, $plugin_default );
 
 		// Simulate theme template existing.
 		// ...????
 
-		$template = $endpoint->template_include( $template_default );
+		$template = $instance->template_include( $template_default );
 
 		// Assert that the theme template is used.
 		// $this->assertSame('/path/to/theme/theme-endpoint-template.php', $template); // ..????
