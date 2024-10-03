40364b5f42716931a7cb487986bf0dbd2cdee400
Anthony Skelton <anthony@anthonyskelton.com>
2024-10-03 09:06:04 -0700

    Fixes Sidebar and Event/Venue Panels opening by default
    * Removes the initialOpen property from PluginDocumentSettingPanel, this slotfill does not allow the property
    * Updates the dependency the PluginDocumentSettingPanel is loaded from. It was moved to '@wordpress/editor'
    * Updates the function to ensure the Event and Venue settings are opened by default. Uses a domReady and first checks if the panel is open, and if it is not then dispatches a toggleEditorPanelOpened() to open the panel.
    * Updates the main domReady in the editor.js file. Removes the duplicate opening of the event settings panel and only checks and opens the general sidebar.

--- src/panels/event-settings/index.js
+++ src/panels/event-settings/index.js
@@ -2,13 +2,14 @@
  * WordPress dependencies.
  */
 import { __ } from '@wordpress/i18n';
-import { dispatch } from '@wordpress/data';
+import domReady from "@wordpress/dom-ready";
+import { dispatch, select } from '@wordpress/data';
 import {
 	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
 	__experimentalVStack as VStack,
 } from '@wordpress/components';
 import { registerPlugin } from '@wordpress/plugins';
-import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
+import { PluginDocumentSettingPanel } from '@wordpress/editor';
 
 /**
  * Internal dependencies.
@@ -41,7 +42,6 @@ const EventSettings = () => {
 			<PluginDocumentSettingPanel
 				name="gatherpress-event-settings"
 				title={__('Event settings', 'gatherpress')}
-				initialOpen={true}
 				className="gatherpress-event-settings"
 			>
 				<VStack spacing={4}>
@@ -76,14 +76,27 @@ registerPlugin('gatherpress-event-settings', {
 /**
  * Toggles the visibility of the 'gatherpress-event-settings' panel in the Block Editor.
  *
- * This function uses the `dispatch` function from the `@wordpress/data` package
- * to toggle the visibility of the 'gatherpress-event-settings' panel in the Block Editor.
- * The panel is identified by the string 'gatherpress-event-settings/gatherpress-event-settings'.
+ * This function ensures that the 'gatherpress-event-settings' panel is open in the WordPress
+ * block editor. It uses the `domReady` function to ensure the DOM is ready before execution.
+ * If the 'gatherpress-event-settings' panel is not open, it opens the panel using the
+ * `toggleEditorPanelOpened` function.
  *
  * @since 1.0.0
  *
  * @return {void}
  */
-dispatch('core/edit-post').toggleEditorPanelOpened(
-	'gatherpress-event-settings/gatherpress-event-settings'
-);
+domReady( () => {
+
+	const selectEditPost = select('core/edit-post');
+	const dispatchEditor = dispatch('core/editor');
+
+	if ( ! selectEditPost || ! dispatchEditor ) {
+		return;
+	}
+
+	const isEventSettingsPanelOpen = selectEditPost.isEditorPanelOpened('gatherpress-event-settings/gatherpress-event-settings');
+
+	if ( ! isEventSettingsPanelOpen ) {
+		dispatchEditor.toggleEditorPanelOpened('gatherpress-event-settings/gatherpress-event-settings');
+	}
+})
