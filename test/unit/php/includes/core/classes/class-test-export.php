7492cf6e806eee3bada19741afb53648ffce64ac
Carsten Bach <mail@carsten-bach.de>
2024-10-05 19:26:18 +0200

    New test for run() & render() methods

--- test/unit/php/includes/core/classes/class-test-export.php
+++ test/unit/php/includes/core/classes/class-test-export.php
@@ -87,7 +87,6 @@ class Test_Export extends Base {
 		$instance = Export::get_instance();
 		$post     = $this->mock->post()->get();
 
-
 		$this->assertEmpty(
 			get_post_meta( $post->ID, Export::POST_META, true ),
 			'Failed to assert the post meta "gatherpress_extend_export" didn\'t exist yet.'
@@ -195,6 +194,51 @@ class Test_Export extends Base {
 			get_post_meta( $post_id, $meta_key ),
 			'Failed to assert the temporary marker was deleted from post meta.'
 		);
+	}
+
+	/**
+	 * Coverage for run & render.
+	 *
+	 * @covers ::run
+	 * @covers ::render
+	 *
+	 * @return void
+	 */
+	public function test_run_and_render(): void {
+
+		$export = Export::get_instance();
+		$post   = $this->mock->post(
+			array(
+				'post_title'   => 'Unit Test Event',
+				'post_type'    => 'gatherpress_event',
+				'post_content' => 'Unit Test description.',
+			)
+		)->get();
+		$event  = new Event( $post->ID );
+		$params = array(
+			'datetime_start' => '2020-05-11 15:00:00',
+			'datetime_end'   => '2020-05-12 17:00:00',
+			'timezone'       => 'America/New_York',
+		);
+		$event->save_datetimes( $params );
+
+		if ( ! function_exists( 'export_wp' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/export.php';
+		}
+
+		$output = Utility::buffer_and_return( 'export_wp', array( array( 'content' => 'gatherpress_event' ) ) );
+
+		$this->assertStringContainsString( '<wp:post_name><![CDATA[unit-test-event]]></wp:post_name>', $output );
+		$this->assertStringContainsString( '<wp:post_type><![CDATA[gatherpress_event]]></wp:post_type>', $output );
+
+		$this->assertStringContainsString( '<wp:meta_key><![CDATA[gatherpress_datetime_start]]></wp:meta_key>', $output );
+		$this->assertStringContainsString( '<wp:meta_value><![CDATA[2020-05-11 15:00:00]]></wp:meta_value>', $output );
+
+		$this->assertStringContainsString( '<wp:meta_key><![CDATA[gatherpress_datetime_end]]></wp:meta_key>', $output );
+		$this->assertStringContainsString( '<wp:meta_value><![CDATA[2020-05-12 17:00:00]]></wp:meta_value>', $output );
+
+		$this->assertStringContainsString( '<wp:meta_key><![CDATA[gatherpress_datetimes]]></wp:meta_key>', $output );
+		$this->assertStringContainsString( '<wp:meta_value><![CDATA[a:5:{s:14:"datetime_start";s:19:"2020-05-11 15:00:00";s:18:"datetime_start_gmt";s:19:"2020-05-11 19:00:00";s:12:"datetime_end";s:19:"2020-05-12 17:00:00";s:16:"datetime_end_gmt";s:19:"2020-05-12 21:00:00";s:8:"timezone";s:16:"America/New_York";}]]></wp:meta_value>', $output );
 	}
 
 	/**
