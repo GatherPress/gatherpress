6bb65de610f8d74767e5e37d46c487c75c8e77e7
Carsten Bach <mail@carsten-bach.de>
2024-10-07 02:44:12 +0200

    Clean up

--- test/e2e/tests/frontend/rsvp.spec.ts
+++ test/e2e/tests/frontend/rsvp.spec.ts
@@ -4,41 +4,6 @@
 const { test, expect } = require('@wordpress/e2e-test-utils-playwright');
 
 test.describe('RSVP to an event', () => {
-	/* 
-	test('02-verify the non-logged in user view RSVP button on home page and perform RSVP action', async ({
-		page,
-	}) => {
-		await page.goto('https://develop.gatherpress.org');
-		await page.getByRole('heading', { name: 'Upcoming Events' }).isVisible();
-		await page
-			.locator('div')
-			.filter({ hasText: /^online Test Event$/ })
-			.isVisible();
-	
-		await page.getByRole('link', { name: 'RSVP' }).first().isVisible();
-	
-		await page.getByRole('link', { name: 'RSVP' }).first().click();
-		await page.getByText('Login', { exact: true }).click();
-	
-		await loginUser({ page, username: 'testuser1' });
-		await page.evaluate(() => window.scrollTo(0, 1000));
-	
-		await page
-			.getByRole('link', { name: 'RSVP' })
-			.first()
-			.click({ timeout: 60000 });
-	
-		await page.locator('a').filter({ hasText: 'Attend' }).click();
-		await page.getByText('Close').click();
-		await page.locator('.gatherpress-rsvp-response__items').first().isVisible(); // verified the RSVP button is visible
-		await expect(page.getByText('Attending').first()).toBeVisible(); // verified the attending text after RSVP action.
-	
-		await page.locator('.gatherpress-rsvp-response__items').first().isVisible(); // verified the attending users list.
-		await page
-			.locator('.gatherpress-rsvp-response__items')
-			.first()
-			.screenshot({ path: 'attending.png' });
-	}); */
 
 	test('A logged in user can perform RSVP action', async ({ page }) => {
 		await page.goto('/');
@@ -46,7 +11,6 @@ test.describe('RSVP to an event', () => {
 		// because "TT5" has an "Events" demo link in its footer.
 		await page.locator('header').getByRole('link', { name: 'Events' }).click({ force: true }); // Imported from of https://github.com/GatherPress/gatherpress-demo-data
 
-		await page.evaluate(() => window.scrollTo(0, 5000));
 		await page
 			.getByRole('link', { name: 'RSVP' })
 			.first()
@@ -63,7 +27,7 @@ test.describe('RSVP to an event', () => {
 
 		await expect(page.getByText('Attending').first()).toBeVisible({
 			timeout: 30000,
-		}); // verified the logged in user perform RSVP action
+		}); // verified the logged in user performed RSVP action
 
 		await expect(
 			page.locator('.gatherpress-rsvp-response__items').first()
