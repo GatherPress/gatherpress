b087736108dba12cf9ed1a22b36ae94b92d21fc9
Carsten Bach <mail@carsten-bach.de>
2024-10-07 03:33:42 +0200

    WIP event tests

--- test/e2e/tests/editor/events.spec.ts
+++ test/e2e/tests/editor/events.spec.ts
@@ -5,28 +5,51 @@ const { test, expect } = require('@wordpress/e2e-test-utils-playwright');
 
 test.describe('Events in the Editor', () => {
 
+	let venueSelector;
+
 	test.beforeEach(async ({ admin, page }) => {
 		await admin.createNewPost({ postType: 'gatherpress_event' });
 		await page.getByLabel('Add title').fill('Change title to allow saving');
-	});
 
-	test.afterEach(async ({ editor, page }) => {
-		await editor.publishPost(); // this is missing the force and doesnt work.
+		venueSelector = await page.getByLabel('Venue Selector');
+		venueSelector.waitFor();
+		await expect( venueSelector ).toBeVisible();
 	});
 
 	test('An admin should be able to publish an online event', async ({
+		editor,
 		page,
+		pageUtils,
 	}) => {
-		await page.getByLabel('Venue Selector').selectOption('Online event');
+
+
+		await venueSelector.selectOption('Online event');
 		await page
 			.getByPlaceholder('Add link to online event')
-			.fill('www.google.com');
+			.fill('www.gatherpress.org');
+
+		await editor.publishPost(); // this is missing the force and doesnt work.
+		// await pageUtils.pressKeys( 'primary+s' );
+		await page.reload();
+
+		await expect( venueSelector ).toBeVisible();
+		await expect( venueSelector ).toHaveText( 'Online event' );
+
 	});
 
 	test('An admin should be able to publish an offline event', async ({
+		editor,
 		page,
 	}) => {
-		await page.getByLabel('Venue Selector').selectOption('Turin'); // Location of WCEU 2024 & imported from https://github.com/GatherPress/demo-data
+		await venueSelector.selectOption('Turin'); // Location of WCEU 2024 & imported from https://github.com/GatherPress/demo-data
+
+		await editor.publishPost(); // this is missing the force and doesnt work.
+		// await pageUtils.pressKeys( 'primary+s' );
+		await page.reload();
+
+		await expect( venueSelector ).toBeVisible();
+		await expect( venueSelector ).toHaveText( 'Turin' );
+
 	});
 
 });
