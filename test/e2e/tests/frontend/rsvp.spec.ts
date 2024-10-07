079cfd266a799d05371d2e8d1a3afa55b083542c
Carsten Bach <mail@carsten-bach.de>
2024-10-08 00:21:48 +0200

    TRY test for non-logged-in users (FAILED)

--- test/e2e/tests/frontend/rsvp.spec.ts
+++ test/e2e/tests/frontend/rsvp.spec.ts
@@ -36,3 +36,42 @@ test.describe('RSVP to an event', () => {
 		).toBeVisible(); // verified the attending users list.
 	});
 });
+
+test.describe('RSVP to an event', () => {
+
+	test.use({ storageState: { cookies: [], origin: [] } });
+
+	// TODO
+
+	test('A NOT logged in user can NOT perform RSVP action', async ({ page }) => {
+		await page.goto('/');
+		// Be explicit witn the header,
+		// because "TT5" has an "Events" demo link in its footer.
+		await page
+			.locator('header')
+			.getByRole('link', { name: 'Events' })
+			.click({ force: true }); // Imported from of https://github.com/GatherPress/gatherpress-demo-data
+
+		await page
+			.getByRole('link', { name: 'RSVP' })
+			.first()
+			.click({ force: true, timeout: 60000 });
+
+		await page
+			.locator('a')
+			.filter({ hasText: 'Attend' })
+			.click({ force: true });
+		await page.getByText('Close').click({ force: true });
+		await expect(
+			page.locator('.gatherpress-rsvp-response__items').first()
+		).toBeVisible(); // verified the RSVP button is visible.
+
+		await expect(page.getByText('Attending').first()).toBeVisible({
+			timeout: 30000,
+		}); // verified the logged in user performed RSVP action
+
+		await expect(
+			page.locator('.gatherpress-rsvp-response__items').first()
+		).toBeVisible(); // verified the attending users list.
+	});
+});
