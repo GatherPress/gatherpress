7a8652553a04dc15c59f200e67f96518f1f7d07b
Mike Auteri <mauteri@gmail.com>
2024-10-10 18:19:01 -0400

    Fixed JS Lint.

--- webpack.config.js
+++ webpack.config.js
@@ -19,16 +19,26 @@ module.exports = [
 			panels: path.resolve(process.cwd(), 'src/panels', 'index.js'),
 			modals: path.resolve(process.cwd(), 'src/modals', 'index.js'),
 			settings: path.resolve(process.cwd(), 'src/settings', 'index.js'),
-			settings_style: path.resolve(process.cwd(), 'src/settings', 'style.scss'),
+			settings_style: path.resolve(
+				process.cwd(),
+				'src/settings',
+				'style.scss'
+			),
 			profile: path.resolve(process.cwd(), 'src/profile', 'index.js'),
-			profile_style: path.resolve(process.cwd(), 'src/profile', 'style.scss'),
+			profile_style: path.resolve(
+				process.cwd(),
+				'src/profile',
+				'style.scss'
+			),
 		},
 		module: {
 			...defaultConfig[0].module,
 			rules: [
 				...defaultConfig[0].module.rules.filter(
-				(rule) =>
-					!/\\\.\(bmp\|png\|jpe\?g\|gif\|webp\)\$/i.test(rule.test.toString())
+					(rule) =>
+						!/\\\.\(bmp\|png\|jpe\?g\|gif\|webp\)\$/i.test(
+							rule.test.toString()
+						)
 				),
 				...[
 					{
