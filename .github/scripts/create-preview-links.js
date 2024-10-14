636f0dd266a04062cfa12ebe5ecf26227a184a4b
Carsten Bach <mail@carsten-bach.de>
2024-10-14 23:46:28 +0200

    Run 'enableMultisite' after the plugin activation! (#950)

--- .github/scripts/create-preview-links.js
+++ .github/scripts/create-preview-links.js
@@ -52,9 +52,6 @@ function createBlueprint(context, number, zipArtifactUrl, phpVersion) {
 		},
 		steps: [
 			{
-				step: 'enableMultisite',
-			},
-			{
 				step: 'login',
 				username: 'admin',
 				password: 'password',
@@ -126,6 +123,16 @@ function createBlueprint(context, number, zipArtifactUrl, phpVersion) {
 					resource: 'url',
 					url: 'https://raw.githubusercontent.com/GatherPress/gatherpress-demo-data/main/GatherPress-demo-data-2024.xml'
 				}
+			},
+			/**
+			 * Run 'enableMultisite' after the plugin activation!
+			 *
+			 * There have been some weird errors with this step, when ran after the login.
+			 * Having it here at the end -kinda- fixes the problem.
+			 * @see https://github.com/GatherPress/gatherpress/issues/950
+			 */
+			{
+				step: 'enableMultisite',
 			},
 		],
 	};
