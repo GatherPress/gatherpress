fdf903c4f8720e0c3464ad4418ee3316d23a130f
Carsten Bach <mail@carsten-bach.de>
2024-10-18 14:00:55 +0200

    BUILD

--- build/blocks/venue/venue.asset.php
+++ build/blocks/venue/venue.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'f1a0db9e615d4c2f9bb7');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'de4b6e5fc31af36075b8');
---
+++ build/variations/add-to-calendar/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blocks', 'wp-i18n', 'wp-primitives'), 'version' => '5527f39fd9a6cf9a1b24');
