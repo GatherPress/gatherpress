b19b471fafa9971d57766c174e8e360982692cfb
Mike Auteri <mauteri@gmail.com>
2024-11-16 15:20:34 -0500

    Updates...

--- build/blocks/modal/index.asset.php
+++ build/blocks/modal/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '430b45880e5e7082e26d');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '4ec754be1937a30d204f');
