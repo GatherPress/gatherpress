3e4d794f5df56ec2ea24400f44d7ccaefe0dcb0a
Mike Auteri <mauteri@gmail.com>
2024-12-06 23:08:31 -0500

    Fix warning in modal content block with background color.

--- build/blocks/modal-content/index.asset.php
+++ build/blocks/modal-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'a2882e3dd9357d415d4c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '92133aa39d658934f363');
