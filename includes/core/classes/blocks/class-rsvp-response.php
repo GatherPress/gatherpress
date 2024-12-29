dbd63952fdf9546f4235b0f92f440c3e6ef35ce2
Mike Auteri <mauteri@gmail.com>
2024-12-29 08:49:37 -0500

    Add postId context to RSVP Response block.

--- includes/core/classes/blocks/class-rsvp-response.php
+++ includes/core/classes/blocks/class-rsvp-response.php
@@ -95,6 +95,7 @@ class Rsvp_Response {
 
 		if ( $tag->next_tag() ) {
 			$tag->set_attribute( 'data-wp-interactive', 'gatherpress/rsvp' );
+			$tag->set_attribute( 'data-wp-context', wp_json_encode( array( 'postId' => get_the_ID() ) ) );
 
 			$responses = (int) $event->rsvp->responses()['attending']['count'];
 
