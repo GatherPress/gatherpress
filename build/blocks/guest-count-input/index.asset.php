a3c6a3f507986aed162e3cfc9b41a98e63e1bdfe
Mike Auteri <mauteri@gmail.com>
2025-01-03 12:03:04 -0500

    Fix build.

--- build/blocks/guest-count-input/index.asset.php
+++ build/blocks/guest-count-input/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c4735c1af1e7789b3206');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'fb2f7a9bfbef6d327537');
--- build/blocks/rsvp-template/index.asset.php
+++ build/blocks/rsvp-template/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0467428fcb07ef9c88ff');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '827f695eba95945faae8');
