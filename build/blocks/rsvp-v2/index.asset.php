edf1a446b2325f943db5d15061ced1819aec3acc
Mike Auteri <mauteri@gmail.com>
2025-01-03 11:54:25 -0500

    Update modal manager in template.

--- build/blocks/rsvp-v2/index.asset.php
+++ build/blocks/rsvp-v2/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'bbba9409f99b5edbe84c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6ddd6ca33144ea449d71');
