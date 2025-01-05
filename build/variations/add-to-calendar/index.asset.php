e62745a3af53922d12ecb9512373dddee8eee559
Mike Auteri <mauteri@gmail.com>
2025-01-05 18:03:16 -0500

    Comment out whole file for now.

--- build/variations/add-to-calendar/index.asset.php
+++ build/variations/add-to-calendar/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '37389dc4747e8e7e0953');
+<?php return array('dependencies' => array(), 'version' => '31d6cfe0d16ae931b73c');
