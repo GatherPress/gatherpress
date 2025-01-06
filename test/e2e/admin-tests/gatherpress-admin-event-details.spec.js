cfb02ee9b981b9459d68a959d50d55c3cad9cb29
Prayag11 <prayagmankar1@gmail.com>
2025-01-06 10:52:52 +0530

    node error resolv

--- test/e2e/admin-tests/gatherpress-admin-event-details.spec.js
+++ test/e2e/admin-tests/gatherpress-admin-event-details.spec.js
@@ -1,4 +1,4 @@
-const { test, expect } = require('@playwright/test');
+const { test } = require('@playwright/test');
 const { login } = require('../reusable-user-steps/common.js');
 
 test.describe('e2e test for event post, verify the event time is visible on front end', () => {
@@ -12,15 +12,24 @@ test.describe('e2e test for event post, verify the event time is visible on fron
 	}) => {
 		await login({ page, username: 'prashantbellad' });
 		await page.getByRole('link', { name: 'Events', exact: true }).click();
-        await page.locator('#wpbody-content').getByRole('link', { name: 'Add New Event' }).click();
-  await page.getByLabel('Add title').fill('event time details');
-  await page.getByRole('button', { name: 'Publish', exact: true }).click();
-  await page.getByLabel('Editor publish').getByRole('button', { name: 'Publish', exact: true }).click();
-  await page.getByLabel('Editor publish').getByRole('link', { name: 'View Event' }).click();
+		await page
+			.locator('#wpbody-content')
+			.getByRole('link', { name: 'Add New Event' })
+			.click();
+		await page.getByLabel('Add title').fill('event time details');
+		await page
+			.getByRole('button', { name: 'Publish', exact: true })
+			.click();
+		await page
+			.getByLabel('Editor publish')
+			.getByRole('button', { name: 'Publish', exact: true })
+			.click();
+		await page
+			.getByLabel('Editor publish')
+			.getByRole('link', { name: 'View Event' })
+			.click();
 
-  await page.locator('#wp--skip-link--target').isVisible();
-    await page.locator('.wp-block-gatherpress-event-date').isVisible();
-
-    
+		await page.locator('#wp--skip-link--target').isVisible();
+		await page.locator('.wp-block-gatherpress-event-date').isVisible();
 	});
 });
