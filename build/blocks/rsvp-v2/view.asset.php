8d77aec4ccc48b4c7f4aecd4a3c058a36db34269
Mike Auteri <mauteri@gmail.com>
2025-01-14 10:45:42 -0500

    Do not automatically change RSVP Response view on RSVP change. Causes issues.

--- build/blocks/rsvp-v2/view.asset.php
+++ build/blocks/rsvp-v2/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '0e2a601a1ef1f0d26d02', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '62b980bec6b89e6d57ce', 'type' => 'module');
