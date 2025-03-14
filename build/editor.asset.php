4fe9d72493eca3aa5bcda26fc948c70a8185fd4f
Mike Auteri <mauteri@gmail.com>
2025-03-14 11:47:30 -0400

    Remove zIndex from block guard overlay. Not needed and causing interference issues with modal and possibly other elements with zIndex.

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '6e693368911464133b47');
+<?php return array('dependencies' => array('moment', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '75097c139e9346e9fb6d');
