bdd57fb47c7b43c1dae50ba1e5491c48c8a37751
Mike Auteri <mauteri@gmail.com>
2025-03-15 22:23:15 -0400

    Fix to event panels not loading properly because not seen as new event.

--- build/blocks/rsvp/index.asset.php
+++ build/blocks/rsvp/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f2b092fdf4f8537e0e54');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '92a29cf7e1d4ca0298ea');
