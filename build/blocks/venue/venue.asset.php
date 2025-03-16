35001338bfc40e982f4bfb6819eec78722c25bcf
Mike Auteri <mauteri@gmail.com>
2025-03-16 13:14:08 -0400

    Node version and package updates.

--- build/blocks/venue/venue.asset.php
+++ build/blocks/venue/venue.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'e36c818d9c8382d7d9c5');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'db3ff690f76f9f978637');
