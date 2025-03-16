a10c6021e8ca69da6d57614d259583975ce6622a
Mike Auteri <mauteri@gmail.com>
2025-03-16 15:24:22 -0400

    Run build.

--- build/blocks/online-event/index.asset.php
+++ build/blocks/online-event/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f0a4a4e49f1f7d0a4213');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c6f6b28104905b0c1a75');
--- build/blocks/rsvp-response/index.asset.php
+++ build/blocks/rsvp-response/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '81f1a1c91661ebfee46f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '59352c6efd1198657431');
