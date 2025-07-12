7710a572669a112d5c5b70cc711c5258120ec283
Mike Auteri <mauteri@gmail.com>
2025-07-12 18:15:29 -0400

    Fix wrong argument.

--- includes/core/classes/class-rsvp.php
+++ includes/core/classes/class-rsvp.php
@@ -234,7 +234,7 @@ class Rsvp {
 		if ( ! empty( $user_id ) ) {
 			$args['user_id'] = $user_id;
 		} elseif ( ! empty( $email ) ) {
-			$args['comment_author_email'] = $email;
+			$args['author_email'] = $email;
 		}
 
 		$rsvp             = $rsvp_query->get_rsvp( $args );
