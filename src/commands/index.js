2d60bfcc69a43576644ad1f30f8b1d6d0d77f0c3
Carsten Bach <mail@carsten-bach.de>
2026-01-22 10:53:59 +0100

    Prepare for rendering components

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,25 +1,37 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
-import { store as commandsStore } from '@wordpress/commands';
-import { dispatch } from '@wordpress/data';
+import {
+	useCommand,
+} from '@wordpress/commands';
 import { mapMarker, calendar } from '@wordpress/icons';
+import { __ } from '@wordpress/i18n';
+import { registerPlugin } from '@wordpress/plugins';
 
-dispatch( commandsStore ).registerCommand( {
-	name: 'gatherpress/add-new-venue',
-	label: __( 'Add new venue', 'gatherpress' ),
-	icon: mapMarker,
-	callback: () => {
-		document.location.href = 'post-new.php?post_type=gatherpress_venue';
-	},
-} );
+const Render = () => {
+	useCommand( {
+		name: 'gatherpress/add-new-venue',
+		label: __( 'Add new venue', 'gatherpress' ),
+		icon: mapMarker,
+		callback: ( { close } ) => {
+			close();
+			document.location.href = 'post-new.php?post_type=gatherpress_venue';
+		},
+	} );
 
-dispatch( commandsStore ).registerCommand( {
-	name: 'gatherpress/add-new-event',
-	label: __( 'Add new event', 'gatherpress' ),
-	icon: calendar,
-	callback: () => {
-		document.location.href = 'post-new.php?post_type=gatherpress_event';
-	},
+	useCommand( {
+		name: 'gatherpress/add-new-event',
+		label: __( 'Add new event', 'gatherpress' ),
+		icon: calendar,
+		callback: ( { close } ) => {
+			close();
+			document.location.href = 'post-new.php?post_type=gatherpress_event';
+		},
+	} );
+
+	return null; // The component doesn't need to render anything visually
+};
+
+registerPlugin( 'gatherpress-commands', {
+	render: Render,
 } );
