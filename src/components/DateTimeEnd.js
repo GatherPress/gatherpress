c5ee2c96e9fc003c44b844e76b5618144d6efc90
Carsten Bach <mail@carsten-bach.de>
2026-01-30 09:12:51 +0100

    Use the site's configured start of the week for all date-time-pickers

--- src/components/DateTimeEnd.js
+++ src/components/DateTimeEnd.js
@@ -16,6 +16,7 @@ import { useDispatch, useSelect } from '@wordpress/data';
 /**
  * Internal dependencies.
  */
+import { getStartOfWeek } from '../helpers/editor';
 import { hasEventPastNotice } from '../helpers/event';
 import {
 	createMomentWithTimezone,
@@ -101,6 +102,7 @@ const DateTimeEnd = () => {
 									)
 								}
 								is12Hour={ is12HourTime }
+								startOfWeek={ getStartOfWeek() }
 							/>
 						) }
 					/>
