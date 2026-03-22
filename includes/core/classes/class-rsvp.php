ba81d145bffacdbddb4410f8f481183c6bbd50d9
Mike Auteri <mauteri@gmail.com>
2026-03-22 16:06:43 -0400

    Simplify code to just no status and not conditional removes.

--- includes/core/classes/class-rsvp.php
+++ includes/core/classes/class-rsvp.php
@@ -308,8 +308,8 @@ class Rsvp {
 			return $data;
 		}
 
-		// If not attending and anonymous or status is 'no_status', remove the record.
-		if ( ( 'not_attending' === $status && $anonymous ) || 'no_status' === $status ) {
+		// If status is 'no_status', remove the record.
+		if ( 'no_status' === $status ) {
 			wp_delete_comment( $comment_id, true );
 
 			wp_cache_delete( sprintf( self::CACHE_KEY, $post_id ), GATHERPRESS_CACHE_GROUP );
