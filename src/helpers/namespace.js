fc6e24c4dbf65636eafca6abdb6c284791435e57
Mike Auteri <mauteri@gmail.com>
2026-04-14 20:30:03 -0400

    Fix spelling.

--- src/helpers/namespace.js
+++ src/helpers/namespace.js
@@ -3,7 +3,7 @@
  *
  * Note: earlier versions of this file also exported post type and taxonomy
  * constants such as CPT_EVENT and TAX_VENUE. Those were intentionally removed
- * in favour of post-type-support checks (post_type_supports / isPostTypeSupporting),
+ * in favor of post-type-support checks (post_type_supports / isPostTypeSupporting),
  * which allow third-party post types to participate in GatherPress features
  * without relying on hardcoded slug comparisons.
  *
