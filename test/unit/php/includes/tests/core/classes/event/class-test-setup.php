294c94f59bd562f50f72f2d8dc9fa53e9a9b9f1b
Carsten Bach <mail@carsten-bach.de>
2026-06-25 19:53:00 +0200

    Adjust unit test for l10n post type slug

--- test/unit/php/includes/tests/core/classes/event/class-test-setup.php
+++ test/unit/php/includes/tests/core/classes/event/class-test-setup.php
@@ -419,7 +419,7 @@ class Test_Setup extends Base {
 		// This also checks that the post type is still registered with the same 'Post Type Singular Name' label,
 		// which is used by the method under test and the test itself.
 		$filter = static function ( string $translation, string $text, string $context ): string {
-			if ( 'Event' !== $text || 'Post Type Singular Name' !== $context ) {
+			if ( 'Event' !== $text || 'Admin menu and post type singular name' !== $context ) {
 				return $translation;
 			}
 			return 'Ünit Tést';
